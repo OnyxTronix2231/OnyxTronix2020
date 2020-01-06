ef81bfc5955a22833ce3c45768e154a40b8c178e
AE173 <s31079@tichon.shohamedu.org.il>
2020-01-06 21:17:51 +0200

    Init

---
+++ src/main/java/robot/Robot.java
@@ -0,0 +1,38 @@
+package robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+
+    @Override
+    public void robotInit() {
+
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+
+    @Override
+    public void testInit() {
+        LiveWindow.setEnabled(true);
+    }
+
+    @Override
+    public void disabledInit() {
+        LiveWindow.setEnabled(false);
+    }
+
+    @Override
+    public void disabledPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+}

87ef0b5ec10863e48df85c2e90ad54d12502353e
AE173 <s31079@tichon.shohamedu.org.il>
2020-01-06 21:26:18 +0200

    test

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
 
     @Override
-    public void robotInit() {
+    public void robotInit() { 
 
     }
 

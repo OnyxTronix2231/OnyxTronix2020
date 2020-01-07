9a41c99de236e9f6e6f7ec4602b4cc695fa1d0e3
polytheking <yuvaly454@gmail.com>
2020-01-07 19:43:15 +0200

    added requirement to collect command

--- src/main/java/robot/ballCollector/commands/Collect.java
+++ src/main/java/robot/ballCollector/commands/Collect.java
@@ -7,13 +7,19 @@ public class Collect extends CommandBase {
 
     private final BallCollector ballCollector;
 
-    public Collect(BallCollector ballCollector) {
+    public Collect(final BallCollector ballCollector) {
         this.ballCollector = ballCollector;
+        addRequirements(ballCollector);
     }
 
     @Override
     public void execute() {
         ballCollector.moveBySpeed(0.5);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
     }
 
     @Override

b67c7bbf523adce6d271fd07c3619cd16584d1a7
Shaked Cohen <shaked113021@gmail.com>
2020-01-07 22:28:47 +0200

    adde button and axis cache to driveJoystick

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -1,14 +1,22 @@
 package robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import onyxTronix.JoystickTriggerFactory;
+import onyxTronix.JoystickTriggerType;
+import onyxTronix.UniqueTriggerCache;
 
 public class Robot extends TimedRobot {
 
     @Override
     public void robotInit() {
-
+        XboxController driveJoystick = new XboxController(0);
+        UniqueTriggerCache driveJoystickButtonCache = new UniqueTriggerCache(driveJoystick,
+          new JoystickTriggerFactory(JoystickTriggerType.Button));
+        UniqueTriggerCache driveJoystickAxisCache = new UniqueTriggerCache(driveJoystick,
+          new JoystickTriggerFactory(JoystickTriggerType.Axis));
     }
 
     @Override

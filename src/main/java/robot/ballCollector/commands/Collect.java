c2020e8f11907dc8b0ca7d978975b410e14a8458
polytheking <yuvaly454@gmail.com>
2020-01-07 19:35:49 +0200

    written Collect command

--- src/main/java/robot/ballCollector/commands/Collect.java
+++ src/main/java/robot/ballCollector/commands/Collect.java
@@ -1,5 +1,23 @@
 package robot.ballCollector.commands;
 
-public class Collect {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import robot.ballCollector.BallCollector;
 
+public class Collect extends CommandBase {
+
+    private final BallCollector ballCollector;
+
+    public Collect(BallCollector ballCollector) {
+        this.ballCollector = ballCollector;
+    }
+
+    @Override
+    public void execute() {
+        ballCollector.moveBySpeed(0.5);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        ballCollector.moveBySpeed(0);
+    }
 }

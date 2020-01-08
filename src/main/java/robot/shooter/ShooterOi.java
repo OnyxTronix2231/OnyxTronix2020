a91024f209eb0ef5682f80fa88947a724c6fed4e
NadaVibe <nadav8775@gmail.com>
2020-01-08 21:45:38 +0200

    Fixed code by conventions

--- src/main/java/robot/shooter/ShooterOi.java
+++ src/main/java/robot/shooter/ShooterOi.java
@@ -1,6 +1,7 @@
 package robot.shooter;
 
-import edu.wpi.first.wpilibj.XboxController;
+import static edu.wpi.first.wpilibj.XboxController.Button;
+
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import onyxTronix.UniqueTriggerCache;
 import robot.shooter.commands.ShootBySpeed;
@@ -8,7 +9,7 @@ import robot.shooter.commands.ShootBySpeed;
 public class ShooterOi {
 
     public ShooterOi(final UniqueTriggerCache buttonsJoystickAxisCache, final Shooter shooter) {
-        final Trigger shootBySpeedButton = buttonsJoystickAxisCache.createJoystickTrigger(XboxController.Button.kA.value);
+        final Trigger shootBySpeedButton = buttonsJoystickAxisCache.createJoystickTrigger(Button.kA.value);
         shootBySpeedButton.whileActiveContinuous(new ShootBySpeed(shooter, () -> ShooterConstants.SPEED));
     }
 }

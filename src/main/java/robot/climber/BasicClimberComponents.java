8f9843b620a5e06bfe337f2144bf633f9cc94bfa
AE173 <s31079@tichon.shohamedu.org.il>
2020-01-09 22:58:29 +0200

    added space

--- src/main/java/robot/climber/BasicClimberComponents.java
+++ src/main/java/robot/climber/BasicClimberComponents.java
@@ -6,11 +6,11 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 public class BasicClimberComponents implements ClimberComponents {
 
     private final WPI_TalonSRX masterMotor;
-
     private final VictorSPX slaveMotor;
 
     public BasicClimberComponents() {
         masterMotor = new WPI_TalonSRX(1);
+
         slaveMotor = new VictorSPX(2);
         slaveMotor.follow(masterMotor);
     }

62c19eee8d2aef454e0da0bffad7ed9e766ff2f7
polytheking <yuvaly454@gmail.com>
2020-01-09 19:55:35 +0200

    fix the code added finals and more....

--- src/main/java/robot/ballCollector/BallCollector.java
+++ src/main/java/robot/ballCollector/BallCollector.java
@@ -1,16 +1,22 @@
 package robot.ballCollector;
 
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class BallCollector extends SubsystemBase {
 
-    private BallCollectorComponents components;
+    private final BallCollectorComponents components;
 
     public BallCollector(final BallCollectorComponents components) {
         this.components = components;
+        CommandScheduler.getInstance().registerSubsystem(this);
     }
 
     public void moveBySpeed(double speed) {
-        components.getMaster().set(speed);
+        components.getMasterMotor().set(speed);
+    }
+
+    public void stopMotor() {
+        moveBySpeed(0);
     }
 }

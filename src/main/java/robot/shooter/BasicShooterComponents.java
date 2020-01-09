d62aec88f4eb86f10ae36454f0a1c3a8cee08c28
NadaVibe <nadav8775@gmail.com>
2020-01-09 18:30:11 +0200

    Changed code by conventions

--- src/main/java/robot/shooter/BasicShooterComponents.java
+++ src/main/java/robot/shooter/BasicShooterComponents.java
@@ -1,13 +1,14 @@
 package robot.shooter;
 
 import com.ctre.phoenix.motorcontrol.IMotorController;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class BasicShooterComponents implements ShooterComponents {
 
     private final WPI_TalonSRX masterMotor;
-    private final IMotorController slaveMotor;
+    private final WPI_VictorSPX slaveMotor;
 
     public BasicShooterComponents() {
         masterMotor = new WPI_TalonSRX(1);

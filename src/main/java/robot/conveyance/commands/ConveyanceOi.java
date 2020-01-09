40fd0f681d29559884a75f65b1ce007a339748d5
yuval <yrashman@gmail.com>
2020-01-09 22:27:43 +0200

    add ConveyanceOi

---
+++ src/main/java/robot/conveyance/commands/ConveyanceOi.java
@@ -0,0 +1,13 @@
+package robot.conveyance.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import onyxTronix.UniqueTriggerCache;
+import robot.conveyance.Conveyance;
+
+public class ConveyanceOi {
+
+  public ConveyanceOi(final Conveyance conveyance, final UniqueTriggerCache buttonJoystickButtonCache){
+
+
+  }
+}

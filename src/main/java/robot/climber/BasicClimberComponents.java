5c55a95ebb98b29873f7dd06d6e998022ea60a71
AE173 <s31079@tichon.shohamedu.org.il>
2020-01-09 23:14:26 +0200

    Edited according to CR (2)

--- src/main/java/robot/climber/BasicClimberComponents.java
+++ src/main/java/robot/climber/BasicClimberComponents.java
@@ -17,11 +17,11 @@ public class BasicClimberComponents implements ClimberComponents {
 
     @Override
     public final WPI_TalonSRX getMasterMotor() {
-        return this.masterMotor;
+        return masterMotor;
     }
 
     @Override
     public final VictorSPX getSlaveMotor() {
-        return this.slaveMotor;
+        return slaveMotor;
     }
 }

0f77bc243a4e85e1adc145f30ece290a0e3eac3c
AE173 <s31079@tichon.shohamedu.org.il>
2020-01-09 16:03:25 +0200

    change to IMotorController in ClimberComponents.java

--- src/main/java/robot/climber/ClimberComponents.java
+++ src/main/java/robot/climber/ClimberComponents.java
@@ -1,8 +1,6 @@
 package robot.climber;
 
 import com.ctre.phoenix.motorcontrol.IMotorController;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public interface ClimberComponents {
 

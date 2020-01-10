4d2484f552c3881059ba46e60415f0c8fef2b86f
AE173 <s31079@tichon.shohamedu.org.il>
2020-01-10 19:54:56 +0200

    correct test

--- src/main/java/robot/climber/BasicClimberComponents.java
+++ src/main/java/robot/climber/BasicClimberComponents.java
@@ -1,15 +1,11 @@
 package robot.climber;
 
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class BasicClimberComponents implements ClimberComponents {
 
   private final WPI_TalonSRX masterMotor;
-
   private final WPI_VictorSPX slaveMotor;
 
   public BasicClimberComponents() {
@@ -18,17 +14,15 @@ public class BasicClimberComponents implements ClimberComponents {
     slaveMotor = new WPI_VictorSPX(2);
     slaveMotor.follow(masterMotor);
 
-    DoubleSolenoid solenoid = new DoubleSolenoid(0, 1);
-    solenoid.set(Value.kForward);
   }
 
   @Override
-  public final WPI_TalonSRX getMaster() {
-    return this.masterMotor;
+  public final WPI_TalonSRX getMasterMotor() {
+    return masterMotor;
   }
 
   @Override
-  public final WPI_VictorSPX getSlave() {
-    return this.slaveMotor;
+  public final WPI_VictorSPX getSlaveMotor() {
+    return slaveMotor;
   }
 }

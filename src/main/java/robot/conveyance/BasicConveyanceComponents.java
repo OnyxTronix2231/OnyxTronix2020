de66c16c050081b67633b2ae5061e114fb2558e5
yuval <yrashman@gmail.com>
2020-01-11 16:40:33 +0200

    delete  imports we dont need

--- src/main/java/robot/conveyance/BasicConveyanceComponents.java
+++ src/main/java/robot/conveyance/BasicConveyanceComponents.java
@@ -1,6 +1,5 @@
 package robot.conveyance;
 
-import com.ctre.phoenix.motorcontrol.IMotorController;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class BasicConveyanceComponents implements ConveyanceComponents {
--- src/main/java/robot/conveyance/ConveyanceComponents.java
+++ src/main/java/robot/conveyance/ConveyanceComponents.java
@@ -1,13 +1,12 @@
 package robot.conveyance;
 
 import com.ctre.phoenix.motorcontrol.IMotorController;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public interface ConveyanceComponents {
 
     WPI_TalonSRX getMasterMotor();
-    
+
     IMotorController getSlaveMotor();
 
 }

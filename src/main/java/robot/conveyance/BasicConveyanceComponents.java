2efbd95f69cec6da5eb5c1f77c27a8c2d405a514
polytheking <yuvaly454@gmail.com>
2020-01-11 22:49:04 +0200

    i did what rashman needed to do...

--- src/main/java/robot/conveyance/BasicConveyanceComponents.java
+++ src/main/java/robot/conveyance/BasicConveyanceComponents.java
@@ -1,5 +1,6 @@
 package robot.conveyance;
 
+import com.ctre.phoenix.motorcontrol.IMotorController;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class BasicConveyanceComponents implements ConveyanceComponents {
@@ -8,9 +9,11 @@ public class BasicConveyanceComponents implements ConveyanceComponents {
     private final WPI_TalonSRX slaveMotor;
 
     public BasicConveyanceComponents() {
-        this.masterMotor = new WPI_TalonSRX(ConveyanceConstants.FIRST_MASTER_MOTOR_PORT);
+        masterMotor = new WPI_TalonSRX(ConveyanceConstants.FIRST_MASTER_MOTOR_PORT);
+        masterMotor.configFactoryDefault();
 
-        this.slaveMotor = new WPI_TalonSRX(ConveyanceConstants.FIRST_SLAVE_MOTOR_PORT);
+        slaveMotor = new WPI_TalonSRX(ConveyanceConstants.FIRST_SLAVE_MOTOR_PORT);
+        slaveMotor.configFactoryDefault();
         slaveMotor.follow(masterMotor);
     }
 
@@ -20,7 +23,7 @@ public class BasicConveyanceComponents implements ConveyanceComponents {
     }
 
     @Override
-    public WPI_TalonSRX getSlaveMotor() {
+    public IMotorController getSlaveMotor() {
         return slaveMotor;
     }
 }
--- src/main/java/robot/conveyance/ConveyanceConstants.java
+++ src/main/java/robot/conveyance/ConveyanceConstants.java
@@ -2,10 +2,10 @@ package robot.conveyance;
 
 public final class ConveyanceConstants {
 
-    public final static double CONVEYANCE_SPEED = 1; //TODO check and change
     public final static int FIRST_MASTER_MOTOR_PORT = 7; //TODO check and change
     public final static int FIRST_SLAVE_MOTOR_PORT = 8; //TODO check and change
     public final static int SECOND_MASTER_MOTOR_PORT = 9; //TODO check and change
     public final static int SECOND_SLAVE_MOTOR_PORT = 10; //TODO check and change
+    public final static double CONVEYANCE_SPEED = 1; //TODO check and change
 
 }

76ca420b1acd3636722258429e9488aac334aae8
polytheking <yuvaly454@gmail.com>
2020-01-11 21:26:45 +0200

    Chang the button to Axis

--- src/main/java/robot/ballCollector/BallCollectorOi.java
+++ src/main/java/robot/ballCollector/BallCollectorOi.java
@@ -1,14 +1,14 @@
 package robot.ballCollector;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import onyxTronix.Axis;
 import onyxTronix.UniqueTriggerCache;
 import robot.ballCollector.commands.CollectBallBySpeed;
 
 public final class BallCollectorOi {
 
     public BallCollectorOi(final BallCollector ballCollector, final UniqueTriggerCache buttonJoystickButtonCache) {
-        final Trigger collectBallButton = buttonJoystickButtonCache.createJoystickTrigger(XboxController.Button.kA.value);
+        final Trigger collectBallButton = buttonJoystickButtonCache.createJoystickTrigger(Axis.kLeftX.value);
         collectBallButton.whileActiveContinuous(new CollectBallBySpeed(ballCollector,
                 () -> BallCollectorConstants.SPEED));
     }

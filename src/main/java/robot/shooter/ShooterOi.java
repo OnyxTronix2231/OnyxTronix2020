e592aefee3e910d80585b88330848e44dc9ed317
NadaVibe <nadav8775@gmail.com>
2020-01-11 23:53:17 +0200

    Made value given to engine change according to the position of the axis

--- src/main/java/robot/shooter/ShooterOi.java
+++ src/main/java/robot/shooter/ShooterOi.java
@@ -1,5 +1,6 @@
 package robot.shooter;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import onyxTronix.Axis;
 import onyxTronix.UniqueTriggerCache;
@@ -8,7 +9,7 @@ import robot.shooter.commands.ShootByPercentOutput;
 public class ShooterOi {
 
   public ShooterOi(final UniqueTriggerCache buttonJoystickAxisCache, final Shooter shooter){
-    final Trigger shootBySpeedButton = buttonJoystickAxisCache.createJoystickTrigger(Axis.kLeftY.value);
-    shootBySpeedButton.whileActiveContinuous(new ShootByPercentOutput(shooter, () -> ShooterConstants.SPEED));
+    final Trigger shootBySpeedAxis = buttonJoystickAxisCache.createJoystickTrigger(Axis.kLeftY.value);
+    shootBySpeedAxis.whileActiveContinuous(new ShootByPercentOutput(shooter, () -> new Joystick(0).getRawAxis(Axis.kLeftY.value)));
   }
 }

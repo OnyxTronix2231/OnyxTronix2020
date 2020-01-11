51fcc5dfca402d63b0434986935df1f267046575
AE173 <s31079@tichon.shohamedu.org.il>
2020-01-11 19:29:31 +0200

    started Turret

---
+++ src/main/java/robot/turret/TurretConstants.java
@@ -0,0 +1,10 @@
+package robot.turret;
+
+public class TurretConstants {
+
+  public static final int MASTER_MOTOR_PORT = 11;
+  public static final double SPEED = 0.2;
+  public static final double CONVERSION_RATE = 4;
+  public static final int ENCODER_UNITS = 1024;
+
+}

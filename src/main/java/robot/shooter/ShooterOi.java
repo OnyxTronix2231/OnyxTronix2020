f528a15db81eac983bf01f9c1d34c61ddbe7c6b6
NadaVibe <nadav8775@gmail.com>
2020-01-12 18:51:38 +0200

    Corrected the axis in ShooterOi

--- src/main/java/robot/shooter/ShooterOi.java
+++ src/main/java/robot/shooter/ShooterOi.java
@@ -2,14 +2,15 @@ package robot.shooter;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import onyxTronix.Axis;
+import onyxTronix.JoystickAxis;
 import onyxTronix.UniqueTriggerCache;
 import robot.shooter.commands.ShootByPercentOutput;
 
 public class ShooterOi {
 
   public ShooterOi(final UniqueTriggerCache buttonJoystickAxisCache, final Shooter shooter){
-    final Trigger shootBySpeedAxis = buttonJoystickAxisCache.createJoystickTrigger(Axis.kLeftY.value);
-    shootBySpeedAxis.whileActiveContinuous(new ShootByPercentOutput(shooter, () -> new Joystick(0).getRawAxis(Axis.kLeftY.value)));
+    final Trigger shootBySpeedAxis = buttonJoystickAxisCache.createJoystickTrigger(JoystickAxis.AxisMap.kLeftY.value);
+    shootBySpeedAxis.whileActiveContinuous(new ShootByPercentOutput(shooter,
+        () -> new Joystick(0).getRawAxis(JoystickAxis.AxisMap.kLeftY.value)));
   }
 }

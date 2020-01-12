b09ee2ac802290fd0c4c44b2c90ff9b3531c28b9
nimrod46 <nimrodma99@gmail.com>
2020-01-12 17:07:40 +0200

    Updated Oi by the new UniqueCache changes

--- src/main/java/robot/shooter/ShooterOi.java
+++ src/main/java/robot/shooter/ShooterOi.java
@@ -1,15 +1,14 @@
 package robot.shooter;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import onyxTronix.Axis;
-import onyxTronix.UniqueTriggerCache;
+import onyxTronix.JoystickAxis;
+import onyxTronix.JoystickAxis.AxisMap;
+import onyxTronix.UniqueAxisCache;
 import robot.shooter.commands.ShootByPercentOutput;
 
 public class ShooterOi {
 
-  public ShooterOi(final UniqueTriggerCache buttonJoystickAxisCache, final Shooter shooter){
-    final Trigger shootBySpeedAxis = buttonJoystickAxisCache.createJoystickTrigger(Axis.kLeftY.value);
-    shootBySpeedAxis.whileActiveContinuous(new ShootByPercentOutput(shooter, () -> new Joystick(0).getRawAxis(Axis.kLeftY.value)));
+  public ShooterOi(final UniqueAxisCache buttonJoystickAxisCache, final Shooter shooter){
+    final JoystickAxis shootBySpeedAxis = buttonJoystickAxisCache.createJoystickTrigger(AxisMap.kLeftY.value);
+    shootBySpeedAxis.whileActiveContinuous(new ShootByPercentOutput(shooter, shootBySpeedAxis::getRawAxis));
   }
 }

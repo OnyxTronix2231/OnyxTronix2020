7b1dd94dd43d615e8224297e9d1e25569b480d51
NadaVibe <nadav8775@gmail.com>
2020-01-13 19:58:36 +0200

    Reformated code by CR

--- src/main/java/robot/shooter/BasicShooterComponents.java
+++ src/main/java/robot/shooter/BasicShooterComponents.java
@@ -25,7 +25,7 @@ public class BasicShooterComponents implements ShooterComponents {
     config.slot0.kP = ShooterConstants.VELOCITY_P;
     config.slot0.kI = ShooterConstants.VELOCITY_I;
     config.slot0.kD = ShooterConstants.VELOCITY_D;
-    config.slot0.kF = ShooterConstants.VELOCITY_F;
+    config.slot0.kF = ShooterConstants.MAX_CLOSED_LOOP_OUTPUT / ShooterConstants.MAX_VELOCITY;
     return config;
   }
 
--- src/main/java/robot/shooter/ShooterConstants.java
+++ src/main/java/robot/shooter/ShooterConstants.java
@@ -8,5 +8,6 @@ public final class ShooterConstants {
   public static final double VELOCITY_P = 0;
   public static final double VELOCITY_I = 0;
   public static final double VELOCITY_D = 0;
-  public static final double VELOCITY_F = 0;
+  public static final double MAX_CLOSED_LOOP_OUTPUT = 1023;
+  public static final double MAX_VELOCITY = 1500; //TODO check and change
 }

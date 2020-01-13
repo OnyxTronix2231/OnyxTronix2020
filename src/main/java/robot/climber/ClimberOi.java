aa038f67b962ecc704f3ae2bf2dd8ffb7b701c79
AE173 <s31079@tichon.shohamedu.org.il>
2020-01-13 16:35:09 +0200

    static imports for Oi

--- src/main/java/robot/climber/ClimberOi.java
+++ src/main/java/robot/climber/ClimberOi.java
@@ -1,6 +1,9 @@
 package robot.climber;
 
 import static edu.wpi.first.wpilibj.XboxController.Button;
+import static edu.wpi.first.wpilibj.XboxController.Button.*;
+import static onyxTronix.JoystickAxis.*;
+import static onyxTronix.JoystickAxis.AxisMap.*;
 
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import onyxTronix.JoystickAxis;
@@ -15,13 +18,13 @@ public class ClimberOi {
   public ClimberOi(final UniqueTriggerCache buttonsJoystickButtonCache,
                    final UniqueAxisCache buttonJoystickAxisCache, final Climber climber) {
     final JoystickAxis climbBySpeedAxis =
-        buttonJoystickAxisCache.createJoystickTrigger(JoystickAxis.AxisMap.kRightY.value);
+        buttonJoystickAxisCache.createJoystickTrigger(kRightY.value);
     climbBySpeedAxis.whileActiveContinuous(new ClimbBySpeed(climber, climbBySpeedAxis::getRawAxis));
 
-    final Trigger openDoubleSolenoidsButton = buttonsJoystickButtonCache.createJoystickTrigger(Button.kB.value);
+    final Trigger openDoubleSolenoidsButton = buttonsJoystickButtonCache.createJoystickTrigger(kB.value);
     openDoubleSolenoidsButton.whenActive(new OpenPistons(climber));
 
-    final Trigger closeDoubleSolenoidsButton = buttonsJoystickButtonCache.createJoystickTrigger(Button.kX.value);
+    final Trigger closeDoubleSolenoidsButton = buttonsJoystickButtonCache.createJoystickTrigger(kX.value);
     closeDoubleSolenoidsButton.whenActive(new ClosePistons(climber));
   }
 }

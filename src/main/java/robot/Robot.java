e32af517ae937cbd18a2fe7c5481a86fa3ffbfb4
CluePen <clu.pen12@gmail.com>
2020-01-13 22:20:39 +0200

    Reverted important files.

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -1,38 +1,50 @@
 package robot;
 
+import static robot.RobotConstants.BUTTONS_JOYSTICK_PORT;
+import static robot.RobotConstants.DRIVE_JOYSTICK_PORT;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import onyxTronix.UniqueAxisCache;
+import onyxTronix.UniqueButtonCache;
 
 public class Robot extends TimedRobot {
 
-  @Override
-  public void robotInit() {
+    @Override
+    public void robotInit() {
+        XboxController driveJoystick = new XboxController(DRIVE_JOYSTICK_PORT);
+        UniqueButtonCache driveJoystickButtonCache = new UniqueButtonCache(driveJoystick);
+        UniqueAxisCache driveJoystickAxisCache = new UniqueAxisCache(driveJoystick);
 
-  }
+        XboxController buttonsJoystick = new XboxController(BUTTONS_JOYSTICK_PORT);
+        UniqueButtonCache buttonsJoystickButtonCache = new UniqueButtonCache(buttonsJoystick);
+        UniqueAxisCache buttonsJoystickAxisCache = new UniqueAxisCache(buttonsJoystick);
+    }
 
-  @Override
-  public void autonomousPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
+    @Override
+    public void autonomousPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
 
-  @Override
-  public void teleopPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
+    @Override
+    public void teleopPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
 
-  @Override
-  public void testInit() {
-    LiveWindow.setEnabled(true);
-  }
+    @Override
+    public void testInit() {
+        LiveWindow.setEnabled(true);
+    }
 
-  @Override
-  public void disabledInit() {
-    LiveWindow.setEnabled(false);
-  }
+    @Override
+    public void disabledInit() {
+        LiveWindow.setEnabled(false);
+    }
 
-  @Override
-  public void disabledPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
+    @Override
+    public void disabledPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
 }

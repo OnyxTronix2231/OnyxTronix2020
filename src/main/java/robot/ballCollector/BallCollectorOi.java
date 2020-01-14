4f7c6a5c4c0dece805d5be3d8e4c12d06ac1f45e
polytheking <yuvaly454@gmail.com>
2020-01-14 19:01:45 +0200

    change buttons to correct buttons

--- src/main/java/robot/ballCollector/BallCollectorOi.java
+++ src/main/java/robot/ballCollector/BallCollectorOi.java
@@ -13,13 +13,13 @@ public final class BallCollectorOi {
 
     public BallCollectorOi(final BallCollector ballCollector, final UniqueAxisCache buttonJoystickAxisCache, UniqueTriggerCache buttonsJoystickButtonCache) {
         final JoystickAxis collectBySpeedAxis =
-                buttonJoystickAxisCache.createJoystickTrigger(JoystickAxis.AxisMap.kLeftY.value);
+                buttonJoystickAxisCache.createJoystickTrigger(JoystickAxis.AxisMap.kLeftTrigger.value);
         collectBySpeedAxis.whileActiveContinuous(new CollectBallBySpeed(ballCollector, collectBySpeedAxis::getRawAxis));
 
         final Trigger openDoubleSolenoidsButton = buttonsJoystickButtonCache.createJoystickTrigger(XboxController.Button.kA.value);
         openDoubleSolenoidsButton.whenActive(new OpenPistons(ballCollector));
 
-        final Trigger closeDoubleSolenoidsButton = buttonsJoystickButtonCache.createJoystickTrigger(XboxController.Button.kY.value);
+        final Trigger closeDoubleSolenoidsButton = buttonsJoystickButtonCache.createJoystickTrigger(XboxController.Button.kB.value);
         closeDoubleSolenoidsButton.whenActive(new ClosePistons(ballCollector));
     }
 }

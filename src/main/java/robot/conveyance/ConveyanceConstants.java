04d635a50f75a6234cb85986b62d3b5c34af1e8a
polytheking <yuvaly454@gmail.com>
2020-01-14 19:55:58 +0200

    didnt do nothing

--- src/main/java/robot/conveyance/ConveyanceConstants.java
+++ src/main/java/robot/conveyance/ConveyanceConstants.java
@@ -2,10 +2,9 @@ package robot.conveyance;
 
 public final class ConveyanceConstants {
 
-    public final static int FIRST_MASTER_MOTOR_PORT = 7; //TODO check and change
-    public final static int FIRST_SLAVE_MOTOR_PORT = 8; //TODO check and change
-    public final static int SECOND_MASTER_MOTOR_PORT = 9; //TODO check and change
-    public final static int SECOND_SLAVE_MOTOR_PORT = 10; //TODO check and change
-    public final static double CONVEYANCE_SPEED = 1; //TODO check and change
-
+    public static final int FIRST_MASTER_MOTOR_PORT = 7; //TODO check and change
+    public static final int FIRST_SLAVE_MOTOR_PORT = 8; //TODO check and change
+    public static final int SECOND_MASTER_MOTOR_PORT = 9; //TODO check and change
+    public static final int SECOND_SLAVE_MOTOR_PORT = 10; //TODO check and change
+    public static final double CONVEYANCE_SPEED = 1; //TODO check and change
 }

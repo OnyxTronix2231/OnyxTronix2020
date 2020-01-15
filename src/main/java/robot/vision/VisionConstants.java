d27a9b66b850ed8d63f981b8fa5da5c84b52b8a5
Shaked Cohen <shaked113021@gmail.com>
2020-01-15 20:32:39 +0200

    removed a constant that is not in use

--- src/main/java/robot/vision/VisionConstants.java
+++ src/main/java/robot/vision/VisionConstants.java
@@ -4,5 +4,4 @@ public final class VisionConstants {
   public static final double KP = 0.06;
   public static final double KI = 0.00;
   public static final double KD = 0.01;
-  public static final double LOOP_TIME = 0.02;
 }

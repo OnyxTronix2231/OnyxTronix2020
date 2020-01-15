36e3c9341b8d46e5a62fcb078f6322744d4fce84
yuval <yrashman@gmail.com>
2020-01-15 23:23:42 +0200

    Hey n.m., we added joystick control just like you asked. please merge us!!! <3

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -9,6 +9,9 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import onyxTronix.UniqueAxisCache;
 import onyxTronix.UniqueButtonCache;
+import robot.drivetrain.BasicDriveTrainComponents;
+import robot.drivetrain.DriveTrain;
+import robot.drivetrain.DriveTrainOi;
 
 public class Robot extends TimedRobot {
 
@@ -21,6 +24,7 @@ public class Robot extends TimedRobot {
         XboxController buttonsJoystick = new XboxController(BUTTONS_JOYSTICK_PORT);
         UniqueButtonCache buttonsJoystickButtonCache = new UniqueButtonCache(buttonsJoystick);
         UniqueAxisCache buttonsJoystickAxisCache = new UniqueAxisCache(buttonsJoystick);
+        new DriveTrainOi(new DriveTrain(new BasicDriveTrainComponents()), driveJoystickAxisCache);
     }
 
     @Override

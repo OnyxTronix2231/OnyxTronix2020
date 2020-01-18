e2add57d125b6a26be048bd15922c53f2cabcf67
yuval <yrashman@gmail.com>
2020-01-18 15:20:53 +0200

    fix according to CR

--- src/main/java/robot/LoaderConveyor/BasicLoaderConveyorComponents.java
+++ src/main/java/robot/LoaderConveyor/BasicLoaderConveyorComponents.java
@@ -1,6 +1,7 @@
 package robot.LoaderConveyor;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import static robot.LoaderConveyor.LoaderConveyorConstants.*;
@@ -9,18 +10,25 @@ import static robot.LoaderConveyor.LoaderConveyorConstants.*;
 public class BasicLoaderConveyorComponents implements LoaderConveyorComponents {
 
     private final WPI_TalonSRX motor;
-    private final Config config;
 
-    public BasicLoaderConveyorComponents(Config config) {
-        this.config = config;
+    public BasicLoaderConveyorComponents() {
         motor = new WPI_TalonSRX(MASTER_MOTOR_PORT);
         motor.configFactoryDefault();
-        motor.configAllSettings(config.getConfiguration());
-        motor.configContinuousCurrentLimit(CONTINUOUS_CURRENT_LIMIT);
-        motor.configPeakCurrentDuration(PICK_AMP_DURATION);
-        motor.configPeakCurrentLimit(PICK_AMP);
+        motor.configAllSettings(getConfiguration());
         motor.setNeutralMode(NeutralMode.Brake);
         motor.enableCurrentLimit(true);
+    }
+
+    private TalonSRXConfiguration getConfiguration() {
+        TalonSRXConfiguration config = new TalonSRXConfiguration();
+        config.slot0.kP = VELOCITY_P;
+        config.slot0.kI = VELOCITY_I;
+        config.slot0.kD = VELOCITY_D;
+        config.slot0.kF = MAX_CLOSED_LOOP_OUTPUT / MAX_VELOCITY;
+        config.continuousCurrentLimit = CONTINUOUS_CURRENT_LIMIT;
+        config.peakCurrentLimit = PICK_AMP;
+        config.peakCurrentDuration = PICK_AMP_DURATION;
+        return config;
     }
 
     @Override

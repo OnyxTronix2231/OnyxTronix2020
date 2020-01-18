6e42d929580582c7ac4f6e4595a69890bdac3135
yuval <yrashman@gmail.com>
2020-01-18 14:52:03 +0200

    create Config

--- src/main/java/robot/LoaderConveyor/BasicLoaderConveyorComponents.java
+++ src/main/java/robot/LoaderConveyor/BasicLoaderConveyorComponents.java
@@ -1,7 +1,6 @@
 package robot.LoaderConveyor;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import static robot.LoaderConveyor.LoaderConveyorConstants.*;
@@ -10,25 +9,18 @@ import static robot.LoaderConveyor.LoaderConveyorConstants.*;
 public class BasicLoaderConveyorComponents implements LoaderConveyorComponents {
 
     private final WPI_TalonSRX motor;
+    private final Config config;
 
-    public BasicLoaderConveyorComponents() {
+    public BasicLoaderConveyorComponents(Config config) {
+        this.config = config;
         motor = new WPI_TalonSRX(MASTER_MOTOR_PORT);
         motor.configFactoryDefault();
-        motor.configAllSettings(getConfiguration());
+        motor.configAllSettings(config.getConfiguration());
         motor.configContinuousCurrentLimit(CONTINUOUS_CURRENT_LIMIT);
         motor.configPeakCurrentDuration(PICK_AMP_DURATION);
         motor.configPeakCurrentLimit(PICK_AMP);
         motor.setNeutralMode(NeutralMode.Brake);
         motor.enableCurrentLimit(true);
-    }
-
-    private TalonSRXConfiguration getConfiguration() {
-        TalonSRXConfiguration config = new TalonSRXConfiguration();
-        config.slot0.kP = VELOCITY_P;
-        config.slot0.kI = VELOCITY_I;
-        config.slot0.kD = VELOCITY_D;
-        config.slot0.kF = MAX_CLOSED_LOOP_OUTPUT / MAX_VELOCITY;
-        return config;
     }
 
     @Override
--- src/main/java/robot/LoaderConveyor/LoaderConveyorComponents.java
+++ src/main/java/robot/LoaderConveyor/LoaderConveyorComponents.java
@@ -1,10 +1,8 @@
 package robot.LoaderConveyor;
 
-import com.ctre.phoenix.motorcontrol.IMotorController;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public interface LoaderConveyorComponents {
 
     WPI_TalonSRX getMotor();
-
 }

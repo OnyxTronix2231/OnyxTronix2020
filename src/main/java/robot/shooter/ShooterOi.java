3e12fe58096f170bcbbe20e23f644452dea84f59
yuval <yrashman@gmail.com>
2020-01-18 22:58:06 +0200

    add axis

--- src/main/java/robot/shooter/ShooterOi.java
+++ src/main/java/robot/shooter/ShooterOi.java
@@ -1,14 +1,14 @@
 package robot.shooter;
 
+import edu.wpi.first.wpilibj.XboxController;
 import onyxTronix.JoystickAxis;
-import onyxTronix.JoystickAxis.AxisMap;
 import onyxTronix.UniqueAxisCache;
 import robot.shooter.commands.ShootByPercentOutput;
 
 public class ShooterOi {
 
   public ShooterOi(final UniqueAxisCache driveJoystickAxisCache, final Shooter shooter){
-    final JoystickAxis shootBySpeedAxis = driveJoystickAxisCache.createJoystickTrigger(AxisMap.kRightTrigger.value);
+    final JoystickAxis shootBySpeedAxis = driveJoystickAxisCache.createJoystickTrigger(XboxController.Axis.kRightTrigger.value);
     shootBySpeedAxis.whileActiveContinuous(new ShootByPercentOutput(shooter, shootBySpeedAxis::getRawAxis));
   }
 }

8dc39b60d99d84df7573b41a6a69c32bcacf9330
polytheking <yuvaly454@gmail.com>
2020-01-20 21:37:20 +0200

    finished Oi

--- src/main/java/robot/crossSubsystem/CrossSubsystemConstants.java
+++ src/main/java/robot/crossSubsystem/CrossSubsystemConstants.java
@@ -2,7 +2,8 @@ package robot.crossSubsystem;
 
 public class CrossSubsystemConstants {
 
-  public static final double MAX_VELOCITY = 1500; //TODO check and change
-
+  public static final double STORAGE_SPEED = 0.8;
+  public static final double MAX_VELOCITY = 1500;
+  public static final double BALL_STOPPER_SPEED = 0.8;
 }
 
--- src/main/java/robot/crossSubsystem/CrossSubsystemOi.java
+++ src/main/java/robot/crossSubsystem/CrossSubsystemOi.java
@@ -1,18 +1,14 @@
 package robot.crossSubsystem;
 
-import static robot.crossSubsystem.CrossSubsystemConstants.MAX_VELOCITY;
-
 import edu.wpi.first.wpilibj.XboxController;
 import onyxTronix.JoystickAxis;
 import onyxTronix.UniqueAxisCache;
 import robot.BallStopper.BallStopper;
 import robot.LoaderConveyor.LoaderConveyor;
-import robot.LoaderConveyor.commands.MoveLoaderByVelocity;
 import robot.StorageConveyor.StorageConveyor;
 import robot.shooter.Shooter;
-import robot.shooter.commands.ShootByVelocity;
 
-import java.util.function.DoubleSupplier;
+import static robot.crossSubsystem.CrossSubsystemConstants.*;
 
 public class CrossSubsystemOi {
 
@@ -21,7 +17,11 @@ public class CrossSubsystemOi {
                           final StorageConveyor storageConveyor,final BallStopper ballStopper) {
     final JoystickAxis shootByVelocityAxis =
             buttonJoystickAxisCache.createJoystickTrigger(XboxController.Axis.kRightTrigger.value);
-    shootByVelocityAxis.whileActiveContinuous(new ShootWithBallStopperTrigger(shooter, loaderConveyor, storageConveyor,
-            ballStopper, ShooterVELOCITY, storageSpeedSupplier)
+    shootByVelocityAxis.whileActiveContinuous(new ShootWithBallStopperTrigger(shooter, loaderConveyor,
+            storageConveyor, ballStopper, () -> MAX_VELOCITY, () -> STORAGE_SPEED, () -> BALL_STOPPER_SPEED));
+
+    buttonJoystickAxisCache.createJoystickTrigger(XboxController.Axis.kLeftTrigger.value);
+    shootByVelocityAxis.whileActiveContinuous(new ShootWithLoaderConveyorTrigger(shooter, loaderConveyor,
+            storageConveyor, ballStopper, () -> MAX_VELOCITY, () -> STORAGE_SPEED, () -> BALL_STOPPER_SPEED));
   }
 }

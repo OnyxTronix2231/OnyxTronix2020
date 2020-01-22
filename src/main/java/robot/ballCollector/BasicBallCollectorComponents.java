57e1c2abc460bc1023e25ed44ce7924d5ec998da
polytheking <yuvaly454@gmail.com>
2020-01-22 20:09:39 +0200

    did imports

--- src/main/java/robot/ballCollector/BasicBallCollectorComponents.java
+++ src/main/java/robot/ballCollector/BasicBallCollectorComponents.java
@@ -1,14 +1,15 @@
 package robot.ballCollector;
 
-import static robot.climber.ClimberConstants.CONTINUOUS_CURRENT_LIMIT;
-import static robot.climber.ClimberConstants.PICK_AMP;
-import static robot.climber.ClimberConstants.PICK_AMP_DURATION;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-
+import static com.ctre.phoenix.motorcontrol.NeutralMode.Brake;
+import static robot.ballCollector.BallCollectorConstants.CONTINUOUS_CURRENT_LIMIT;
+import static robot.ballCollector.BallCollectorConstants.DOUBLE_SOLENOID_FORWARD_PORT;
+import static robot.ballCollector.BallCollectorConstants.DOUBLE_SOLENOID_REVERSE_PORT;
+import static robot.ballCollector.BallCollectorConstants.MASTER_MOTOR_PORT;
 import static robot.ballCollector.BallCollectorConstants.PICK_AMP;
 import static robot.ballCollector.BallCollectorConstants.PICK_AMP_DURATION;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class BasicBallCollectorComponents implements BallCollectorComponents {
 
@@ -16,16 +17,16 @@ public class BasicBallCollectorComponents implements BallCollectorComponents {
     private final DoubleSolenoid doubleSolenoid;
 
     public BasicBallCollectorComponents() {
-        masterMotor = new WPI_TalonSRX(BallCollectorConstants.MASTER_MOTOR_PORT);
+        masterMotor = new WPI_TalonSRX(MASTER_MOTOR_PORT);
         masterMotor.configFactoryDefault();
-        masterMotor.configPeakCurrentLimit(BallCollectorConstants.PICK_AMP);
-        masterMotor.configPeakCurrentDuration(BallCollectorConstants.PICK_AMP_DURATION);
-        masterMotor.configContinuousCurrentLimit(BallCollectorConstants.CONTINUOUS_CURRENT_LIMIT);
+        masterMotor.configPeakCurrentLimit(PICK_AMP);
+        masterMotor.configPeakCurrentDuration(PICK_AMP_DURATION);
+        masterMotor.configContinuousCurrentLimit(CONTINUOUS_CURRENT_LIMIT);
         masterMotor.enableCurrentLimit(true);
-        masterMotor.setNeutralMode(NeutralMode.Brake);
+        masterMotor.setNeutralMode(Brake);
         masterMotor.setInverted(true);
 
-        doubleSolenoid = new DoubleSolenoid(BallCollectorConstants.DOUBLE_SOLENOID_FORWARD_PORT, BallCollectorConstants.DOUBLE_SOLENOID_REVERSE_PORT);
+        doubleSolenoid = new DoubleSolenoid(DOUBLE_SOLENOID_FORWARD_PORT, DOUBLE_SOLENOID_REVERSE_PORT);
     }
 
     @Override

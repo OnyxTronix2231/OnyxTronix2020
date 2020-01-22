1a0c5bb6579689e40404dbf4b16b24fdfee2ed79
polytheking <yuvaly454@gmail.com>
2020-01-22 19:56:49 +0200

    did combo for the all shooter stuff

--- src/main/java/robot/crossSubsystem/CrossSubsystemOi.java
+++ src/main/java/robot/crossSubsystem/CrossSubsystemOi.java
@@ -4,8 +4,10 @@ import static robot.crossSubsystem.CrossSubsystemConstants.MAX_VELOCITY;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import onyxTronix.JoystickAxis;
 import onyxTronix.UniqueAxisCache;
+import onyxTronix.UniqueButtonCache;
 import robot.BallStopper.BallStopper;
 import robot.LoaderConveyor.LoaderConveyor;
 import robot.StorageConveyor.StorageConveyor;
@@ -15,16 +17,19 @@ import static robot.crossSubsystem.CrossSubsystemConstants.*;
 
 public class CrossSubsystemOi {
 
-  public CrossSubsystemOi(final UniqueAxisCache buttonJoystickAxisCache,
+  public CrossSubsystemOi(final UniqueButtonCache driveJoystickButtonCache,
                           final Shooter shooter, final LoaderConveyor loaderConveyor,
                           final StorageConveyor storageConveyor,final BallStopper ballStopper) {
-    final JoystickAxis shootByVelocityAxis =
-            buttonJoystickAxisCache.createJoystickTrigger(XboxController.Axis.kRightTrigger.value);
-    shootByVelocityAxis.whileActiveContinuous(new ShootWithBallStopperTrigger(shooter, loaderConveyor,
-            storageConveyor, ballStopper, () -> MAX_VELOCITY, () -> STORAGE_SPEED, () -> BALL_STOPPER_SPEED));
+    final JoystickButton shootWithBallStopperTrigger =
+            driveJoystickButtonCache.createJoystickTrigger(XboxController.Button.kY.value);
+    final JoystickButton shootWithLoaderConveyorTrigger =
+    driveJoystickButtonCache.createJoystickTrigger(XboxController.Button.kStickLeft.value);
 
-    buttonJoystickAxisCache.createJoystickTrigger(XboxController.Axis.kLeftTrigger.value);
-    shootByVelocityAxis.whileActiveContinuous(new ShootWithLoaderConveyorTrigger(shooter, loaderConveyor,
+    shootWithBallStopperTrigger.and(shootWithLoaderConveyorTrigger.negate()).whileActiveContinuous
+        (new ShootWithBallStopperTrigger(shooter, loaderConveyor,
+            storageConveyor, ballStopper, () -> MAX_VELOCITY, () -> STORAGE_SPEED, () -> BALL_STOPPER_SPEED));
+    shootWithLoaderConveyorTrigger.and(shootWithBallStopperTrigger).whileActiveContinuous(
+        new ShootWithLoaderConveyorTrigger(shooter, loaderConveyor,
             storageConveyor, ballStopper, () -> MAX_VELOCITY, () -> STORAGE_SPEED, () -> BALL_STOPPER_SPEED));
   }
 }

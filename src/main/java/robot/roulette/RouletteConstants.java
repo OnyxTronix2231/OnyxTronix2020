ba258fe3558a70752212e3a759fd986b1beda2b8
milki <udbe2222@gmail.com>
2020-01-28 20:16:06 +0200

    moved a line to another line

--- src/main/java/robot/roulette/RouletteConstants.java
+++ src/main/java/robot/roulette/RouletteConstants.java
@@ -11,6 +11,7 @@ public final class RouletteConstants {
     public static final double K_D = 0;
     public static final int MAX_VELOCITY = 800;
     public static final int MAX_ACCELERATION = 800;
+    public static final int MOTIONCURVESTRENGH = 0;// todo changeeeeee!!!!!
     public static final double MAX_CLOSED_LOOP_OUTPUT = 1023;
     public final static double SPIN_COLOR_SPEED = 0.3;
     public final static double SPIN_ROTATIONS_SPEED = 0.7;
@@ -31,5 +32,4 @@ public final class RouletteConstants {
     public final static RGBValue BLUE_RGB_VALUE = new RGBValue(0.108, 0.428, 0.462);
     public final static RGBValue RED_RGB_VALUE = new RGBValue(0.551, 0.329, 0.119);
     public final static RGBValue YELLOW_RGB_VALUE = new RGBValue(0.319, 0.571, 0.108);
-    public static final int MOTIONCURVESTRENGH = 0;// todo changeeeeee!!!!!
 }

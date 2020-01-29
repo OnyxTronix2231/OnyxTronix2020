daf925024d6905e96c25bfdc724aead099a321be
Shaked Cohen <shaked113021@gmail.com>
2020-01-29 09:10:28 +0200

    removed unnecessary imports

--- src/main/java/robot/vision/commands/CorrectRobotToTarget.java
+++ src/main/java/robot/vision/commands/CorrectRobotToTarget.java
@@ -1,12 +1,7 @@
 package robot.vision.commands;
 
-import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import robot.drivetrain.DriveTrain;
 import robot.drivetrain.commands.RotateToNavXOffset;
-import robot.vision.VisionConstants;
 import robot.vision.VisionTarget;
 import java.util.function.Supplier;
 

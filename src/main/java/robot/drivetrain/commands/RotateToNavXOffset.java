57bc87f31a9e17bc56563a205535e7dbf1a42bf2
Shaked Cohen <shaked113021@gmail.com>
2020-01-29 13:03:26 +0200

    removed unnecessary logic

--- src/main/java/robot/drivetrain/commands/RotateToNavXOffset.java
+++ src/main/java/robot/drivetrain/commands/RotateToNavXOffset.java
@@ -6,19 +6,6 @@ import java.util.function.DoubleSupplier;
 
 public class RotateToNavXOffset extends RotateToAngleNavX {
   public RotateToNavXOffset(DriveTrain driveTrain, DoubleSupplier angleSupplier) {
-    super(driveTrain, () ->
-    {
-      final double targetAngle = driveTrain.getNavXYaw() + angleSupplier.getAsDouble();
-      if(targetAngle == -180) {
-        return 180;
-      }
-      if(targetAngle > 180) {
-        return -360 + targetAngle;
-      }
-      if(targetAngle < -180) {
-        return 360 + targetAngle;
-      }
-      return targetAngle;
-    });
+    super(driveTrain, () -> driveTrain.getNavXYaw() + angleSupplier.getAsDouble());
   }
 }

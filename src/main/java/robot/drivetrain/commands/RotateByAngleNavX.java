43e713d79e5aa85ea0f2fd72e4a5a39a0df8f2b4
Shaked Cohen <shaked113021@gmail.com>
2020-01-29 14:46:53 +0200

    Refactored Name Of RotateToNavXOffset.java to RotateByAngleNavX

--- src/main/java/robot/drivetrain/commands/RotateByAngleNavX.java
+++ src/main/java/robot/drivetrain/commands/RotateByAngleNavX.java
@@ -4,8 +4,8 @@ import robot.drivetrain.DriveTrain;
 
 import java.util.function.DoubleSupplier;
 
-public class RotateToNavXOffset extends RotateToAngleNavX {
-  public RotateToNavXOffset(DriveTrain driveTrain, DoubleSupplier angleSupplier) {
+public class RotateByAngleNavX extends RotateToAngleNavX {
+  public RotateByAngleNavX(DriveTrain driveTrain, DoubleSupplier angleSupplier) {
     super(driveTrain, () -> driveTrain.getNavXYaw() + angleSupplier.getAsDouble());
   }
 }

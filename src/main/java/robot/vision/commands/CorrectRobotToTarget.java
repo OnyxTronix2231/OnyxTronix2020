98bc91fcad95e23d89a2b15d01155b7f609df507
Shaked Cohen <shaked113021@gmail.com>
2020-01-29 14:46:53 +0200

    Refactored Name Of RotateToNavXOffset.java to RotateByAngleNavX

--- src/main/java/robot/vision/commands/CorrectRobotToTarget.java
+++ src/main/java/robot/vision/commands/CorrectRobotToTarget.java
@@ -1,11 +1,11 @@
 package robot.vision.commands;
 
 import robot.drivetrain.DriveTrain;
-import robot.drivetrain.commands.RotateToNavXOffset;
+import robot.drivetrain.commands.RotateByAngleNavX;
 import robot.vision.VisionTarget;
 import java.util.function.Supplier;
 
-public class CorrectRobotToTarget extends RotateToNavXOffset {
+public class CorrectRobotToTarget extends RotateByAngleNavX {
 
   public CorrectRobotToTarget(final DriveTrain driveTrain, final Supplier<VisionTarget> visionTargetSupplier) {
     super(driveTrain, () -> visionTargetSupplier.get().getHorizontalOffset());

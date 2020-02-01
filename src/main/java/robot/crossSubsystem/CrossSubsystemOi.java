22855bcb1687cac0e9fd38cfbd0a6e7db4a0a88b
yuval <yrashman@gmail.com>
2020-02-01 20:04:51 +0200

    calculate pid

--- src/main/java/robot/crossSubsystem/CrossSubsystemOi.java
+++ src/main/java/robot/crossSubsystem/CrossSubsystemOi.java
@@ -1,13 +1,14 @@
 package robot.crossSubsystem;
 
-import static robot.crossSubsystem.CrossSubsystemConstants.MAX_VELOCITY;
-
 import edu.wpi.first.wpilibj.XboxController;
 import onyxTronix.JoystickAxis;
 import onyxTronix.UniqueAxisCache;
 import robot.LoaderConveyor.LoaderConveyor;
+import robot.LoaderConveyor.LoaderConveyorConstants;
 import robot.LoaderConveyor.commands.MoveLoaderByVelocity;
 import robot.shooter.Shooter;
+import robot.shooter.ShooterConstants;
+import robot.shooter.commands.ShootByPercentOutput;
 import robot.shooter.commands.ShootByVelocity;
 
 public class CrossSubsystemOi {
@@ -17,7 +18,7 @@ public class CrossSubsystemOi {
     final JoystickAxis shootByVelocityAxis =
         driveJoystickAxisCache.createJoystickTrigger(XboxController.Axis.kRightTrigger.value);
     shootByVelocityAxis.whileActiveContinuous(new ShootByVelocity(shooter,
-        () -> shootByVelocityAxis.getRawAxis() * MAX_VELOCITY)
-        .alongWith(new MoveLoaderByVelocity(loaderConveyor, () -> shootByVelocityAxis.getRawAxis() * MAX_VELOCITY)));
+        () -> 42000)
+        .alongWith(new MoveLoaderByVelocity(loaderConveyor, () -> shootByVelocityAxis.getRawAxis() * LoaderConveyorConstants.MAX_VELOCITY)));
   }
 }

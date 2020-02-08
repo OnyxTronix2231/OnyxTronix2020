2253ce3f437e80784c7a6164ba6657df1c447462
polytheking <yuvaly454@gmail.com>
2020-02-08 23:01:18 +0200

    fix things

--- src/main/java/robot/shooter/ShooterConstants.java
+++ src/main/java/robot/shooter/ShooterConstants.java
@@ -2,14 +2,15 @@ package robot.shooter;
 
 public final class ShooterConstants {
 
-  public static final int MASTER_PORT = 4;
-  public static final int SLAVE_PORT = 5;
   public static final double SPEED = 0.5; //TODO check and change
   public static final double VELOCITY_P = 0;//TODO check and change
   public static final double VELOCITY_I = 0;//TODO check and change
   public static final double VELOCITY_D = 0;//TODO check and change
   public static final double MAX_CLOSED_LOOP_OUTPUT = 1023;//TODO check and change
   public static final double MAX_VELOCITY = 1500; //TODO check and change
+  public static final double TOLERANCE = 0.5; //TODO check and change
+  public static final int MASTER_PORT = 4;
+  public static final int SLAVE_PORT = 5;
   public static final int PICK_AMP = 40;//TODO check and change
   public static final int PICK_AMP_DURATION = 0;//TODO check and change
   public static final int CONTINUOUS_CURRENT_LIMIT = 20; // TODO: check and change

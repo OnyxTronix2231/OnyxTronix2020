294026910e045150ed343284060649862829db04
yuval <yrashman@gmail.com>
2020-02-09 20:56:23 +0200

    fix spaces

--- src/main/java/robot/StorageConveyor/StorageConveyorConstants.java
+++ src/main/java/robot/StorageConveyor/StorageConveyorConstants.java
@@ -2,11 +2,11 @@ package robot.StorageConveyor;
 
 public final class StorageConveyorConstants {
 
-  public static final class StorageConveyorComponents{
+  public static final class StorageConveyorComponents {
 
-     static final int MASTER_MOTOR_PORT = 9; //TODO: change to correct value.
-     static final int CONTINUOUS_CURRENT_LIMIT = 20; //TODO check and change
-     static final int PICK_AMP_DURATION = 0; //TODO check and change
-     static final int PICK_AMP = 40; //TODO check and change
+    static final int MASTER_MOTOR_PORT = 9; //TODO: change to correct value.
+    static final int CONTINUOUS_CURRENT_LIMIT = 20; //TODO check and change
+    static final int PICK_AMP_DURATION = 0; //TODO check and change
+    static final int PICK_AMP = 40; //TODO check and change
   }
 }

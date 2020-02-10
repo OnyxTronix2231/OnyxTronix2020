0e8517acfa73a32daec145557efa00c498738c81
nimrod46 <nimrodma99@gmail.com>
2020-02-10 22:44:31 +0200

    Revent irellevant changes

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -3,32 +3,12 @@ package robot;
 import static robot.RobotConstants.BUTTONS_JOYSTICK_PORT;
 import static robot.RobotConstants.DRIVE_JOYSTICK_PORT;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import onyxTronix.UniqueAxisCache;
 import onyxTronix.UniqueButtonCache;
-import robot.LoaderConveyor.BasicLoaderConveyorComponents;
-import robot.LoaderConveyor.LoaderConveyor;
-import robot.ballCollector.BallCollector;
-import robot.ballCollector.BallCollectorOi;
-import robot.ballCollector.BasicBallCollectorComponents;
-import robot.crossSubsystem.CrossSubsystemOi;
-import robot.drivetrain.BasicDriveTrainComponents;
-import robot.drivetrain.DriveTrain;
-import robot.drivetrain.commands.DriveBySpeed;
-import robot.shooter.BasicShooterComponents;
-import robot.shooter.Shooter;
-import robot.shooter.ShooterOi;
-import robot.turret.BasicTurretComponents;
-import robot.vision.VisionCalculations;
-import robot.vision.VisionOi;
-import robot.vision.limelight.Limelight;
-import robot.yawControl.YawControl;
-import robot.yawControl.YawControlOi;
 
 public class Robot extends TimedRobot {
 
@@ -41,30 +21,6 @@ public class Robot extends TimedRobot {
         XboxController buttonsJoystick = new XboxController(BUTTONS_JOYSTICK_PORT);
         UniqueButtonCache buttonsJoystickButtonCache = new UniqueButtonCache(buttonsJoystick);
         UniqueAxisCache buttonsJoystickAxisCache = new UniqueAxisCache(buttonsJoystick);
-//        BallCollector ballCollector = new BallCollector(new BasicBallCollectorComponents());
-//        new BallCollectorOi(ballCollector, buttonsJoystickAxisCache, buttonsJoystickButtonCache);
-
-        LoaderConveyor loaderConveyor =  new LoaderConveyor(new BasicLoaderConveyorComponents());
-        Shooter shooter = new Shooter(new BasicShooterComponents());
-        new CrossSubsystemOi(driveJoystickAxisCache, driveJoystickButtonCache, shooter, loaderConveyor);
-
-        DriveTrain driveTrain = new DriveTrain(new BasicDriveTrainComponents());
-        driveTrain.setDefaultCommand(new DriveBySpeed(driveTrain,
-            () -> driveJoystick.getY(GenericHID.Hand.kLeft), () -> -driveJoystick.getX(GenericHID.Hand.kRight)));
-
-        Shuffleboard.getTab("Shooter").addNumber("Limelight Angle", this::getLimelightAngle);
-        Shuffleboard.getTab("Shooter").addNumber("Limelight Distance", this::getLimelightDistance);
-    }
-
-    public double getLimelightAngle() {
-        if(Limelight.getInstance().targetFound()) {
-            return Limelight.getInstance().getTarget().getHorizontalOffsetToCrosshair();
-        }
-        return 0;
-    }
-
-    public double getLimelightDistance() {
-        return VisionCalculations.calculateDistance(Limelight.getInstance().getTarget());
     }
 
     @Override

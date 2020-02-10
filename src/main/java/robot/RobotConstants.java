85cf50578ef77860f42dc618dfe6c6a50ae3a8d5
milki <udbe2222@gmail.com>
2020-02-10 19:47:51 +0200

    `revented files

--- src/main/java/robot/RobotConstants.java
+++ src/main/java/robot/RobotConstants.java
@@ -1,6 +1,6 @@
 package robot;
 
 public final class RobotConstants {
-    public static final int DRIVE_JOYSTICK_PORT = 0;
-    public static final int BUTTONS_JOYSTICK_PORT = 1;
+  public static final int DRIVE_JOYSTICK_PORT = 0;
+  public static final int BUTTONS_JOYSTICK_PORT = 1;
 }

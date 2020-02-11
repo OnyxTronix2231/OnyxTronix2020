09b0c0f81757f7d865a605dba8c17e4aece992b5
Shaked Cohen <shaked113021@gmail.com>
2020-02-11 23:15:51 +0200

    CHECK and CHANGE

--- src/main/java/robot/vision/VisionConstants.java
+++ src/main/java/robot/vision/VisionConstants.java
@@ -2,7 +2,7 @@ package robot.vision;
 
 public final class VisionConstants {
   public static final double TARGET_HEIGHT_CM = 230; // 227.98 in competition
-  public static final double TARGET_LENGTH_CM = 43.2; // TODO: Check and change
+  public static final double TARGET_LENGTH_CM = 43.2;
   public static final double TARGET_WIDTH_CM = 100;
   public static final double TARGET_LENGTH_WIDTH_RATIO_CM = TARGET_LENGTH_CM / TARGET_WIDTH_CM;
   public static final double CAMERA_VERTICAL_OFFSET_ANGLE = 27.3; // TODO: Check and change

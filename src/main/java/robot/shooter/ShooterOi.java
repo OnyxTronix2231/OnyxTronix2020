e64069baab133f6c4b8448f9d56d786ef986fb06
YuvalRashman <yrashman@gmail.com>
2020-02-11 13:41:36 +0200

    Organized imports

--- src/main/java/robot/shooter/ShooterOi.java
+++ src/main/java/robot/shooter/ShooterOi.java
@@ -2,12 +2,8 @@ package robot.shooter;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import onyxTronix.JoystickAxis;
 import onyxTronix.UniqueAxisCache;
 import onyxTronix.UniqueButtonCache;
-import onyxTronix.UniqueTriggerCache;
-import robot.ballCollector.BallCollector;
-import robot.ballCollector.commands.CollectBallBySpeed;
 import robot.shooter.commands.ShootByPercentOutput;
 
 public class ShooterOi {

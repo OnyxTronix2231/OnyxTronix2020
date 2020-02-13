e03d913ba8de1d236f9418e4b46e88de7e9f018c
AE173 <s31079@tichon.shohamedu.org.il>
2020-02-13 18:33:27 +0200

    changed to main Joystick

--- src/main/java/robot/ballCollector/BallCollectorOi.java
+++ src/main/java/robot/ballCollector/BallCollectorOi.java
@@ -5,24 +5,24 @@ import static robot.ballCollector.BallCollectorConstants.SPEED;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import onyxTronix.UniqueAxisCache;
-import onyxTronix.UniqueTriggerCache;
+import onyxTronix.UniqueButtonCache;
 import robot.ballCollector.commands.ClosePistons;
 import robot.ballCollector.commands.CollectBallBySpeed;
 import robot.ballCollector.commands.OpenPistons;
 
 public final class BallCollectorOi {
 
-  public BallCollectorOi(final BallCollector ballCollector, final UniqueAxisCache buttonJoystickAxisCache,
-                         final UniqueTriggerCache buttonsJoystickButtonCache) {
+  public BallCollectorOi(final BallCollector ballCollector, final UniqueAxisCache driverJoystickAxisCache,
+                         final UniqueButtonCache driverJoystickButtonCache) {
     final Trigger collectBySpeed =
-        buttonJoystickAxisCache.createJoystickTrigger(XboxController.Button.kB.value);
+        driverJoystickAxisCache.createJoystickTrigger(XboxController.Button.kB.value);
     collectBySpeed.whileActiveContinuous(new CollectBallBySpeed(ballCollector, ()-> SPEED));
 
-    final Trigger openPistonsButton = buttonsJoystickButtonCache.createJoystickTrigger
+    final Trigger openPistonsButton = driverJoystickButtonCache.createJoystickTrigger
         (XboxController.Button.kBumperRight.value);
     openPistonsButton.whenActive(new OpenPistons(ballCollector));
 
-    final Trigger closePistonsButton = buttonsJoystickButtonCache.createJoystickTrigger
+    final Trigger closePistonsButton = driverJoystickButtonCache.createJoystickTrigger
         (XboxController.Button.kBumperLeft.value);
     closePistonsButton.whenActive(new ClosePistons(ballCollector));
   }

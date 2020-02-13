1e415be5135ab95ac09533d43a42bcc9908aebcd
NadaVibe <nadav8775@gmail.com>
2020-02-13 18:48:38 +0200

    Added a button by the buttons table

--- src/main/java/robot/storageConveyor/StorageConveyorConstants.java
+++ src/main/java/robot/storageConveyor/StorageConveyorConstants.java
@@ -8,5 +8,6 @@ public final class StorageConveyorConstants {
     static final int CONTINUOUS_CURRENT_LIMIT = 20; //TODO check and change
     static final int PEAK_AMP_DURATION = 0; //TODO check and change
     static final int PEAK_AMP = 40; //TODO check and change
+    static final double ENGINE_SPEED = 0.5;
   }
 }

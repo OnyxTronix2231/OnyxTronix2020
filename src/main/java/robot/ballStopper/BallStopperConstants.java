3317d3d91bef5cea5387fc38af8e9615e444bcff
Shaked Cohen <27896110+shaked113021@users.noreply.github.com>
2020-02-13 21:22:50 +0200

    Update src/main/java/robot/ballStopper/BallStopperConstants.java

--- src/main/java/robot/ballStopper/BallStopperConstants.java
+++ src/main/java/robot/ballStopper/BallStopperConstants.java
@@ -5,7 +5,7 @@ public class BallStopperConstants {
   public final static class BallStopperComponentsA {
     final static int MASTER_MOTOR_PORT = 9; //TODO: change to correct value.
     static final int PEAK_AMP_DURATION = 0; //TODO check and change
-    public static final int CONTINUOUS_CURRENT_LIMIT = 20; //TODO check and change
+    static final int CONTINUOUS_CURRENT_LIMIT = 20; //TODO check and change
     public static final int PEAK_AMP = 40; //TODO check and change
   }
 }

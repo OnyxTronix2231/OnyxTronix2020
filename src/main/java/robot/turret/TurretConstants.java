2eaa02a0c3b55b706a40eb00ef61dfbbac0d3d0d
NadaVibe <nadav8775@gmail.com>
2020-02-13 21:55:39 +0200

    Reformatted code by CR

--- src/main/java/robot/turret/TurretConstants.java
+++ src/main/java/robot/turret/TurretConstants.java
@@ -2,17 +2,6 @@ package robot.turret;
 
 final class TurretConstants {
 
-  static final int DEGREES_IN_CIRCLE = 360;
-  static final int TOLERANCE = 10;
-  static final int FLIP_POINT = 180;
-  static final int MAX_ANGLE = 135;
-  static final int MIN_ANGLE = -135;
-  static final int REVERTED_MAX_ANGLE = -225;
-  static final int REVERTED_MIN_ANGLE = 225;
-  private static final int ENCODER_UNITS = 1023;
-  private static final double CONVERSION_RATE = 1;
-  static final double ENCODER_TO_ANGLE = DEGREES_IN_CIRCLE / (ENCODER_UNITS * CONVERSION_RATE);
-
   static final class TurretComponentsA {
     static final int MASTER_MOTOR_PORT = 11; //TODO check and change
     static final int PEAK_AMP = 40; //TODO check and change
@@ -26,4 +15,15 @@ final class TurretConstants {
     static final double VELOCITY_D = 2; //TODO check and change
     static final double VELOCITY_F = (double) MAX_OUTPUT / MAX_VELOCITY;
   }
+
+  static final int DEGREES_IN_CIRCLE = 360;
+  static final int TOLERANCE = 10;
+  static final int FLIP_POINT = 180;
+  static final int MAX_ANGLE = 135;
+  static final int MIN_ANGLE = -135;
+  static final int REVERTED_MAX_ANGLE = -225;
+  static final int REVERTED_MIN_ANGLE = 225;
+  private static final int ENCODER_UNITS = 1023;
+  private static final double CONVERSION_RATE = 1;
+  static final double ENCODER_TO_ANGLE = DEGREES_IN_CIRCLE / (ENCODER_UNITS * CONVERSION_RATE);
 }

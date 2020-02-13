a4a113748f26abb487e825df46654f2d20268acd
NadaVibe <nadav8775@gmail.com>
2020-02-13 19:17:13 +0200

    Added an axis for checks

--- src/main/java/robot/turret/TurretOi.java
+++ src/main/java/robot/turret/TurretOi.java
@@ -1,9 +1,17 @@
 package robot.turret;
 
+import edu.wpi.first.wpilibj.XboxController;
+import onyxTronix.JoystickAxis;
 import onyxTronix.UniqueAxisCache;
+import robot.turret.commands.ChangeAngleOffsetByPercent;
 
 public class TurretOi {
 
   public TurretOi(final UniqueAxisCache buttonJoystickAxisCache, final Turret turret) {
+
+    final JoystickAxis changeAngleByPercentAxis =
+        buttonJoystickAxisCache.createJoystickTrigger(XboxController.Axis.kRightX.value);
+    changeAngleByPercentAxis.whileActiveContinuous(new ChangeAngleOffsetByPercent(turret,
+        () -> changeAngleByPercentAxis.getRawAxis()));
   }
 }

7c3ea38710cb5854603750a9fe966b892fc22c14
NadaVibe <nadav8775@gmail.com>
2020-02-15 17:42:42 +0200

    Revert "Reformatted code by CR"
    This reverts commit bde64e209270cb52e4c27596c0560736b86d3e26.

--- src/main/java/robot/roulette/Roulette.java
+++ src/main/java/robot/roulette/Roulette.java
@@ -16,6 +16,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
+
 public class Roulette extends SubsystemBase {
 
   private final RouletteComponents components;

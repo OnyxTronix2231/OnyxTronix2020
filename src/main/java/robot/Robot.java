c3e1c321a9fcb5da102dfa0ac0a70706a8ef1a00
Shaked Cohen <shaked113021@gmail.com>
2020-02-15 23:53:08 +0200

    temporarily disabled all subsystems except drivetrain

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -65,12 +65,18 @@ public class Robot extends TimedRobot {
 
     if (ROBOT_TYPE == RobotType.A) {
       driveTrainComponents = new BasicDriveTrainComponentsA();
-      ballCollectorComponents = new BasicBallCollectorComponentsA();
-      storageConveyorComponents = new BasicStorageConveyorComponentsA();
-      ballStopperComponents = new BasicBallStopperComponentsA();
-      turretComponents = new BasicTurretComponentsA();
-      loaderConveyorComponents = new BasicLoaderConveyorComponentsA();
-      shooterComponents = new BasicShooterComponentsA();
+//      ballCollectorComponents = new BasicBallCollectorComponentsA();
+      ballCollectorComponents = null;
+//      storageConveyorComponents = new BasicStorageConveyorComponentsA();
+      storageConveyorComponents = null;
+//      ballStopperComponents = new BasicBallStopperComponentsA();
+      ballStopperComponents = null;
+//      turretComponents = new BasicTurretComponentsA();
+      turretComponents = null;
+//      loaderConveyorComponents = new BasicLoaderConveyorComponentsA();
+      loaderConveyorComponents = null;
+//      shooterComponents = new BasicShooterComponentsA();
+      shooterComponents = null;
     } else {
       driveTrainComponents = null; //TODO: use BasicDriveTrainComponentsB Here
       ballCollectorComponents = null; //TODO: use BasicBallCollectorComponentsB Here
@@ -84,23 +90,23 @@ public class Robot extends TimedRobot {
     final DriveTrain driveTrain = new DriveTrain(driveTrainComponents);
     driveTrain.setDefaultCommand(new DriveByJoystick(driveTrain, driveJoystick));
 
-    final BallCollector ballCollector = new BallCollector(ballCollectorComponents);
-    new BallCollectorOi(ballCollector, driveJoystickAxisCache, driveJoystickButtonCache);
-
-    final StorageConveyor storageConveyor = new StorageConveyor(storageConveyorComponents);
-    new StorageConveyorOi(storageConveyor, buttonsJoystickButtonCache);
-
-    final BallStopper ballStopper = new BallStopper(ballStopperComponents);
-    new BallStopperOi(ballStopper, buttonsJoystickButtonCache);
-
-    final Turret turret = new Turret(turretComponents);
-    new TurretOi(turret, buttonsJoystickAxisCache);
-
-    final LoaderConveyor loaderConveyor = new LoaderConveyor(loaderConveyorComponents);
-    new LoaderConveyorOi(loaderConveyor, buttonsJoystickButtonCache);
-
-    final Shooter shooter = new Shooter(shooterComponents);
-    new ShooterOi(buttonsJoystickAxisCache, buttonsJoystickButtonCache, shooter);
+//    final BallCollector ballCollector = new BallCollector(ballCollectorComponents);
+//    new BallCollectorOi(ballCollector, driveJoystickAxisCache, driveJoystickButtonCache);
+//
+//    final StorageConveyor storageConveyor = new StorageConveyor(storageConveyorComponents);
+//    new StorageConveyorOi(storageConveyor, buttonsJoystickButtonCache);
+//
+//    final BallStopper ballStopper = new BallStopper(ballStopperComponents);
+//    new BallStopperOi(ballStopper, buttonsJoystickButtonCache);
+//
+//    final Turret turret = new Turret(turretComponents);
+//    new TurretOi(turret, buttonsJoystickAxisCache);
+//
+//    final LoaderConveyor loaderConveyor = new LoaderConveyor(loaderConveyorComponents);
+//    new LoaderConveyorOi(loaderConveyor, buttonsJoystickButtonCache);
+//
+//    final Shooter shooter = new Shooter(shooterComponents);
+//    new ShooterOi(buttonsJoystickAxisCache, buttonsJoystickButtonCache, shooter);
   }
 
   @Override

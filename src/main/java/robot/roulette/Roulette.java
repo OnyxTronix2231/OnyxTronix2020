7fbad7c5051fc8b693e56eede607350fbc0c2e40
Shaked Cohen <shaked113021@gmail.com>
2020-02-15 14:10:53 +0200

    reformatted code

--- src/main/java/robot/roulette/Roulette.java
+++ src/main/java/robot/roulette/Roulette.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -18,99 +19,99 @@ import java.util.Optional;
 
 public class Roulette extends SubsystemBase {
 
-    private final RouletteComponents components;
-    private final List<RouletteColor> rouletteColors;
-    double rouletteRotation = 0;
+  private final RouletteComponents components;
+  private final List<RouletteColor> rouletteColors;
+  double rouletteRotation = 0;
 
-    public Roulette(final RouletteComponents components) {
-        this.components = components;
-        this.rouletteColors = Arrays.asList(RouletteColor.values());
+  public Roulette(final RouletteComponents components) {
+    this.components = components;
+    this.rouletteColors = Arrays.asList(RouletteColor.values());
+  }
+
+  public void moveRouletteBySpeed(final double speed) {
+    components.getMasterMotor().set(speed);
+  }
+
+  public void openPistons() {
+    components.getDoubleRightSolenoid().set(DoubleSolenoid.Value.kForward);
+    components.getDoubleLeftSolenoid().set(DoubleSolenoid.Value.kForward);
+  }
+
+  public void closePistons() {
+    components.getDoubleRightSolenoid().close();
+    components.getDoubleLeftSolenoid().close();
+  }
+
+  public RouletteColor getRequiredColorFromMatchColor() {
+    RouletteColor requiredMatchColor;
+    if (DriverStation.getInstance().getGameSpecificMessage().isEmpty()) {
+      return null;
     }
-
-    public void moveRouletteBySpeed(final double speed) {
-        components.getMasterMotor().set(speed);
+    switch (DriverStation.getInstance().getGameSpecificMessage().charAt(0)) {
+      case 'B':
+        requiredMatchColor = RouletteColor.Blue;
+        break;
+      case 'G':
+        requiredMatchColor = RouletteColor.Green;
+        break;
+      case 'R':
+        requiredMatchColor = RouletteColor.Red;
+        break;
+      case 'Y':
+        requiredMatchColor = RouletteColor.Yellow;
+        break;
+      default:
+        requiredMatchColor = null;
+        break;
     }
+    return requiredMatchColor;
+  }
 
-    public void openPistons() {
-        components.getDoubleRightSolenoid().set(DoubleSolenoid.Value.kForward);
-        components.getDoubleLeftSolenoid().set(DoubleSolenoid.Value.kForward);
-    }
+  public RouletteColor getCurrentColor() {
+    final Color detectedColor = components.getColorSensorV3().getColor();
+    final RouletteColor[] rouletteColors = RouletteColor.values();
+    Optional<RouletteColor> roulette = Arrays.stream(rouletteColors).max(
+        Comparator.comparing(color -> color.getRgbValue().howCloseTo(detectedColor)));
+    return roulette.orElse(null);
+  }
 
-    public void closePistons() {
-        components.getDoubleRightSolenoid().close();
-        components.getDoubleLeftSolenoid().close();
-    }
+  public void stopMotor() {
+    moveRouletteBySpeed(0);
+  }
 
-    public RouletteColor getRequiredColorFromMatchColor() {
-        RouletteColor requiredMatchColor;
-        if (DriverStation.getInstance().getGameSpecificMessage().isEmpty()) {
-            return null;
-        }
-        switch (DriverStation.getInstance().getGameSpecificMessage().charAt(0)) {
-            case 'B':
-                requiredMatchColor = RouletteColor.Blue;
-                break;
-            case 'G':
-                requiredMatchColor = RouletteColor.Green;
-                break;
-            case 'R':
-                requiredMatchColor = RouletteColor.Red;
-                break;
-            case 'Y':
-                requiredMatchColor = RouletteColor.Yellow;
-                break;
-            default:
-                requiredMatchColor = null;
-                break;
-        }
-        return requiredMatchColor;
-    }
+  public void spinByColorsCount(final double requiredColorCount) {
+    rouletteRotation = getRouletteRotationByColorCount(requiredColorCount);
+    getRouletteRotationToEncoderUnits(getRouletteRotationByColorCount(requiredColorCount));
+  }
 
-    public RouletteColor getCurrentColor() {
-        final Color detectedColor = components.getColorSensorV3().getColor();
-        final RouletteColor[] rouletteColors = RouletteColor.values();
-        Optional<RouletteColor> roulette = Arrays.stream(rouletteColors).max(
-            Comparator.comparing(color -> color.getRgbValue().howCloseTo(detectedColor)));
-        return roulette.orElse(null);
-    }
+  public double getRouletteRotationToEncoderUnits(final double rouletteRotations) {
+    return rouletteRotations * ENCODER_UNITS_PER_ROTATION * ROULETTE_ROTATION_TO_WHEEL_ROTATION;
+  }
 
-    public void stopMotor() {
-        moveRouletteBySpeed(0);
-    }
+  public double getRouletteRotationByColorCount(final double colorCount) {
+    return colorCount / COLORS_IN_ROTATIONS;
+  }
 
-    public void spinByColorsCount(final double requiredColorCount) {
-        rouletteRotation = getRouletteRotationByColorCount(requiredColorCount);
-        getRouletteRotationToEncoderUnits(getRouletteRotationByColorCount(requiredColorCount));
-    }
+  /**
+   * @return When return value is positive, the roulette needs to be rotated clockwise
+   */
 
-    public double getRouletteRotationToEncoderUnits(final double rouletteRotations) {
-        return rouletteRotations * ENCODER_UNITS_PER_ROTATION * ROULETTE_ROTATION_TO_WHEEL_ROTATION;
-    }
+  public double getColorCountRequiredToColor(final RouletteColor requiredColor) {
+    int indexOfRequiredColor = rouletteColors.indexOf(requiredColor);
+    int indexOfCurrentColor = rouletteColors.indexOf(getCurrentColor());
+    int colorDistance = Math.abs(indexOfRequiredColor - indexOfCurrentColor);
+    return colorDistance - DISTANCE_FROM_FIELD_SENSOR;
+  }
 
-    public double getRouletteRotationByColorCount(final double colorCount) {
-        return colorCount / COLORS_IN_ROTATIONS;
-    }
+  public void resetEncoder() {
+    components.getMasterMotor().setSelectedSensorPosition(0);
+  }
 
-    /**
-     * @return When return value is positive, the roulette needs to be rotated clockwise
-     */
+  public boolean isOnTarget() {
+    return Math.abs(components.getMasterMotor().getClosedLoopError()) <= TOLERANCE;
+  }
 
-    public double getColorCountRequiredToColor(final RouletteColor requiredColor) {
-        int indexOfRequiredColor = rouletteColors.indexOf(requiredColor);
-        int indexOfCurrentColor = rouletteColors.indexOf(getCurrentColor());
-        int colorDistance = Math.abs(indexOfRequiredColor - indexOfCurrentColor);
-        return colorDistance - DISTANCE_FROM_FIELD_SENSOR;
-    }
-
-    public void resetEncoder() {
-        components.getMasterMotor().setSelectedSensorPosition(0);
-    }
-
-    public boolean isOnTarget() {
-        return Math.abs(components.getMasterMotor().getClosedLoopError()) <= TOLERANCE;
-    }
-
-    public boolean isOnTargetColor(final double colorsRequired){
-        return colorsRequired == 0;
-    }
+  public boolean isOnTargetColor(final double colorsRequired) {
+    return colorsRequired == 0;
+  }
 }

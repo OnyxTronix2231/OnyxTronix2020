013cca9ba47db7f61bcc6a9f49a74a17ca9d839d
NadaVibe <nadav8775@gmail.com>
2020-02-15 17:43:07 +0200

    Reformatted code by CR

--- src/main/java/robot/roulette/Roulette.java
+++ src/main/java/robot/roulette/Roulette.java
@@ -16,7 +16,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
-
 public class Roulette extends SubsystemBase {
 
   private final RouletteComponents components;

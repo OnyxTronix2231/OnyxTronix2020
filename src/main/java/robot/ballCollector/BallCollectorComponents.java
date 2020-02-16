a60ee3d507a6dd66418d8f20950c733b8e6c3e5e
Shaked Cohen <shaked113021@gmail.com>
2020-02-16 19:54:06 +0200

    removed unnecessary import

--- src/main/java/robot/ballCollector/BallCollectorComponents.java
+++ src/main/java/robot/ballCollector/BallCollectorComponents.java
@@ -1,6 +1,5 @@
 package robot.ballCollector;
 
-import com.ctre.phoenix.motorcontrol.IMotorController;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 

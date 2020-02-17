8dc40ce361084d6be3c13d3f81324dd7d9bcf259
NadaVibe <nadav8775@gmail.com>
2020-02-17 21:20:41 +0200

    Fixed YawControl buttons by the buttons table

--- src/main/java/robot/yawControl/YawControlOi.java
+++ src/main/java/robot/yawControl/YawControlOi.java
@@ -2,33 +2,34 @@ package robot.yawControl;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import onyxTronix.JoystickAxis;
 import onyxTronix.UniqueAxisCache;
 import onyxTronix.UniqueButtonCache;
 import robot.drivetrain.DriveTrain;
 import robot.vision.target.ConditionalTargetMaker;
-import robot.vision.target.VisionTargetFactory;
 import robot.yawControl.commands.AlignByVisionOrOdometryAndVision;
 import robot.yawControl.commands.SetTurretState;
 
 public class YawControlOi {
   public YawControlOi(final YawControl yawControl, final DriveTrain driveTrain,
                       final ConditionalTargetMaker targetMaker, final UniqueButtonCache buttonJoystickButtonCache,
-                      final UniqueAxisCache buttonsJoystickAxisCache) {
+                      final UniqueAxisCache buttonsJoystickAxisCache,
+                      final UniqueButtonCache driverJoystickButtonCache) {
     final JoystickButton alignByToTargetButton =
-        buttonJoystickButtonCache.createJoystickTrigger(XboxController.Button.kY.value);
+        driverJoystickButtonCache.createJoystickTrigger(XboxController.Button.kY.value);
     alignByToTargetButton.whenPressed(new AlignByVisionOrOdometryAndVision(yawControl, driveTrain, targetMaker::makeTarget));
 
-    final JoystickButton setStateRTFButton = buttonJoystickButtonCache
-        .createJoystickTrigger(XboxController.Button.kBumperLeft.value);
-    setStateRTFButton.whenPressed(new SetTurretState(yawControl, YawControl.TurretState.RTF));
+    final JoystickAxis setStateRTFButton = buttonsJoystickAxisCache
+        .createJoystickTrigger(XboxController.Axis.kLeftTrigger.value);
+    setStateRTFButton.whenActive(new SetTurretState(yawControl, YawControl.TurretState.RTF));
 
-    final JoystickButton setStateRTRButton = buttonJoystickButtonCache
-        .createJoystickTrigger(XboxController.Button.kBumperRight.value);
-    setStateRTRButton.whenPressed(new SetTurretState(yawControl, YawControl.TurretState.RTR));
+    final JoystickAxis setStateRTRButton = buttonsJoystickAxisCache
+        .createJoystickTrigger(XboxController.Axis.kRightTrigger.value);
+    setStateRTRButton.whenActive(new SetTurretState(yawControl, YawControl.TurretState.RTR));
 
     final JoystickButton setStateHOMINGButton = buttonJoystickButtonCache
-        .createJoystickTrigger(XboxController.Button.kA.value);
-    setStateHOMINGButton.whenPressed(new SetTurretState(yawControl, YawControl.TurretState.HOMING));
+        .createJoystickTrigger(XboxController.Button.kStickRight.value);
+    setStateHOMINGButton.whenActive(new SetTurretState(yawControl, YawControl.TurretState.HOMING));
   }
 }
 

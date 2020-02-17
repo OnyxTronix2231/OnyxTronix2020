d713674ecee9e1095e71ccdcd7573ac2c1a0ab7b
yuval <yrashman@gmail.com>
2020-02-17 21:01:40 +0200

    fix stuff

--- src/main/java/robot/crossSubsystem/CrossSubsystemConstants.java
+++ src/main/java/robot/crossSubsystem/CrossSubsystemConstants.java
@@ -4,6 +4,11 @@ public class CrossSubsystemConstants {
 
   public static final double STORAGE_SPEED = 0.8; //TODO check and change
   public static final double BALL_STOPPER_SPEED = 0.8; //TODO check and change
+  public static final double LOADER_CONVEYOR_SPEED = 0.8; //TODO check and change
   public static final double SPIN_SHOOTER_AND_LOADER_TIME_OUT = 3; //TODO check and change
+  public static final double TIME_BETWEEN_BALLS = 100; //TODO check and  change if need
+  public static final double DELAY_AFTER_SHOOT = 0; //TODO check and  change if need
+  public static final double LOADER_DELAY = 0.3; //TODO check and  change if need
+
 }
 
--- src/main/java/robot/crossSubsystem/SmartShooterOi.java
+++ src/main/java/robot/crossSubsystem/SmartShooterOi.java
@@ -1,6 +1,7 @@
 package robot.crossSubsystem;
 
 import static robot.crossSubsystem.CrossSubsystemConstants.BALL_STOPPER_SPEED;
+import static robot.crossSubsystem.CrossSubsystemConstants.LOADER_CONVEYOR_SPEED;
 import static robot.crossSubsystem.CrossSubsystemConstants.SPIN_SHOOTER_AND_LOADER_TIME_OUT;
 import static robot.crossSubsystem.CrossSubsystemConstants.STORAGE_SPEED;
 
@@ -8,12 +9,13 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import onyxTronix.UniqueButtonCache;
 import robot.ballStopper.BallStopper;
-import robot.crossSubsystem.commands.ShootWithBallStopperByDistance;
-import robot.crossSubsystem.commands.ShootWithLoaderConveyorByDistance;
+import robot.crossSubsystem.commands.MoveConveyorsByBallStopperTrigger;
+import robot.crossSubsystem.commands.MoveConveyorsByLoaderConveyorTrigger;
 import robot.crossSubsystem.commands.SpinShooterAndLoaderByDistance;
 import robot.loaderConveyor.LoaderConveyor;
 import robot.loaderConveyor.commands.StopLoaderConveyor;
 import robot.shooter.Shooter;
+import robot.shooter.commands.ShootByDistance;
 import robot.shooter.commands.StopShooter;
 import robot.storageConveyor.StorageConveyor;
 import robot.vision.VisionCalculations;
@@ -30,20 +32,32 @@ public class SmartShooterOi {
         () -> VisionCalculations.calculateDistance(target))
         .withTimeout(SPIN_SHOOTER_AND_LOADER_TIME_OUT));
 
-    final JoystickButton shootWithLoaderConveyorByDistance =
+    final JoystickButton moveConveyorsByLoaderConveyorTriggerButton =
         driveJoystickButtonCache.createJoystickTrigger(XboxController.Button.kStickLeft.value);
     final JoystickButton shootWithBallStopperByDistance =
         driveJoystickButtonCache.createJoystickTrigger(XboxController.Button.kX.value);
 
-    shootWithBallStopperByDistance.and(shootWithLoaderConveyorByDistance.negate()).
+    shootWithBallStopperByDistance.and(moveConveyorsByLoaderConveyorTriggerButton.negate()).
+        whileActiveContinuous(new SpinShooterAndLoaderByDistance(shooter,loaderConveyor,
+            () -> VisionCalculations.calculateDistance(target)));
+
+    shootWithBallStopperByDistance.and(moveConveyorsByLoaderConveyorTriggerButton.negate()).
         whileActiveContinuous
-            (new ShootWithBallStopperByDistance(shooter, loaderConveyor,
-                storageConveyor, ballStopper, () -> VisionCalculations.calculateDistance(target),
-                () -> STORAGE_SPEED, () -> BALL_STOPPER_SPEED))
+            (new MoveConveyorsByBallStopperTrigger(shooter, loaderConveyor,
+                storageConveyor, ballStopper, () -> STORAGE_SPEED,
+                 () -> BALL_STOPPER_SPEED))
         .whenInactive(new StopShooter(shooter).alongWith(new StopLoaderConveyor(loaderConveyor)));
-    shootWithLoaderConveyorByDistance.and(shootWithBallStopperByDistance).whileActiveContinuous(
-        new ShootWithLoaderConveyorByDistance(shooter, loaderConveyor,
-            storageConveyor, ballStopper, () -> VisionCalculations.calculateDistance(target),
-            () -> STORAGE_SPEED, () -> BALL_STOPPER_SPEED));
+
+
+    moveConveyorsByLoaderConveyorTriggerButton.and(shootWithBallStopperByDistance).
+        whileActiveContinuous(new ShootByDistance(shooter,
+            () -> VisionCalculations.calculateDistance(target)));
+
+    moveConveyorsByLoaderConveyorTriggerButton.and(shootWithBallStopperByDistance).whileActiveContinuous(
+        new MoveConveyorsByLoaderConveyorTrigger(shooter, loaderConveyor,
+            storageConveyor, ballStopper, () -> LOADER_CONVEYOR_SPEED,
+            () -> STORAGE_SPEED, () -> BALL_STOPPER_SPEED)).whenInactive(new StopShooter(shooter));
+
+
   }
 }

a28a5ac64aa78b34d87d902c7fb7aafd183c388b
yuval <yrashman@gmail.com>
2020-02-22 21:32:17 +0200

    did ctrl alt l

--- src/main/java/robot/delayConveyor/BasicDelayConveyorComponentsA.java
+++ src/main/java/robot/delayConveyor/BasicDelayConveyorComponentsA.java
@@ -1,9 +1,9 @@
 package robot.delayConveyor;
 
 import static robot.delayConveyor.DelayConveyorConstants.DelayConveyorComponentsA.MASTER_MOTOR_PORT;
+
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
 
 public class BasicDelayConveyorComponentsA implements DelayConveyorComponents {
 
@@ -15,9 +15,9 @@ public class BasicDelayConveyorComponentsA implements DelayConveyorComponents {
     motor.setNeutralMode(NeutralMode.Brake);
   }
 
-    @Override
-    public WPI_VictorSPX getMotor() {
+  @Override
+  public WPI_VictorSPX getMotor() {
     return motor;
   }
-  }
+}
 
--- src/main/java/robot/delayConveyor/DelayConveyor.java
+++ src/main/java/robot/delayConveyor/DelayConveyor.java
@@ -4,17 +4,17 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DelayConveyor extends SubsystemBase {
 
-    private final DelayConveyorComponents components;
+  private final DelayConveyorComponents components;
 
-    public DelayConveyor(final DelayConveyorComponents delayConveyorComponents) {
-      this.components = delayConveyorComponents;
-    }
-
-    public void moveDelayConveyorBySpeed(final double speed) {
-      components.getMotor().set(speed);
-    }
-
-    public void stopMotor() {
-      moveDelayConveyorBySpeed(0);
-    }
+  public DelayConveyor(final DelayConveyorComponents delayConveyorComponents) {
+    this.components = delayConveyorComponents;
   }
+
+  public void moveDelayConveyorBySpeed(final double speed) {
+    components.getMotor().set(speed);
+  }
+
+  public void stopMotor() {
+    moveDelayConveyorBySpeed(0);
+  }
+}

8e00d893b81b2965db0f99446e91723fa701431e
AE173 <s31079@tichon.shohamedu.org.il>
2020-02-23 18:47:37 +0200

    Removed all of Vaisgur's idiotic mistakes

--- src/main/java/robot/crossSubsystem/commands/MoveConveyorsByLoaderAsTrigger.java
+++ src/main/java/robot/crossSubsystem/commands/MoveConveyorsByLoaderAsTrigger.java
@@ -1,17 +1,11 @@
 package robot.crossSubsystem.commands;
 
-import static robot.crossSubsystem.CrossSubsystemConstants.DELAY_AFTER_SHOOT;
-import static robot.crossSubsystem.CrossSubsystemConstants.LOADER_DELAY;
-import static robot.crossSubsystem.CrossSubsystemConstants.TIME_BETWEEN_BALLS;
 import static robot.crossSubsystem.CrossSubsystemConstants.WAIT_FOR_VELOCITY;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import robot.ballStopper.BallStopper;
 import robot.ballStopper.commands.MoveBallStopperBySpeed;
-import robot.ballStopper.commands.MoveBallStopperBySpeedWithTime;
 import robot.loaderConveyor.LoaderConveyor;
-import robot.loaderConveyor.commands.MoveLoaderBySpeedWithTime;
 import robot.loaderConveyor.commands.MoveLoaderConveyorBySpeed;
 import robot.shooter.Shooter;
 import robot.shooter.commands.WaitUntilShooterVelocityOnTarget;
@@ -29,11 +23,12 @@ public class MoveConveyorsByLoaderAsTrigger extends ParallelCommandGroup {
                                         final DoubleSupplier ballStopperSpeedSupplier) {
     super(sequence(
         new WaitUntilShooterVelocityOnTarget(shooter, WAIT_FOR_VELOCITY),
-        sequence(sequence(
-            new MoveLoaderBySpeedWithTime(loaderConveyor, loaderSpeed, 0.2) ,
-            new MoveBallStopperBySpeedWithTime(ballStopper, ballStopperSpeedSupplier, 0.2)),
-            parallel(new MoveStorageConveyorBySpeed(storageConveyor, storageSpeedSupplier),
-                new MoveLoaderBySpeedWithTime(loaderConveyor, loaderSpeed, 0.2),
-                new MoveBallStopperBySpeedWithTime(ballStopper, ballStopperSpeedSupplier, 0.2)))));
+        sequence(
+            new MoveLoaderConveyorBySpeed(loaderConveyor, loaderSpeed).withTimeout(0.2) ,
+            new MoveBallStopperBySpeed(ballStopper, ballStopperSpeedSupplier).withTimeout(0.2)),
+            parallel(
+                new MoveStorageConveyorBySpeed(storageConveyor, storageSpeedSupplier),
+                new MoveLoaderConveyorBySpeed(loaderConveyor, loaderSpeed).withTimeout(0.2),
+                new MoveBallStopperBySpeed(ballStopper, ballStopperSpeedSupplier).withTimeout(0.2))));
   }
 }

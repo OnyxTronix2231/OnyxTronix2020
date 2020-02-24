77585663fb01f64654b5592770f0a1be31f09282
yuval <yrashman@gmail.com>
2020-02-24 21:29:31 +0200

    delete imports

--- src/main/java/robot/shooter/Shooter.java
+++ src/main/java/robot/shooter/Shooter.java
@@ -1,11 +1,7 @@
 package robot.shooter;
 
 import static robot.RobotConstants.PRIMARY_PID;
-import static robot.shooter.ShooterConstants.CLOSE_SOLENOID_VALUE;
 import static robot.shooter.ShooterConstants.IS_PISTON_OPEN;
-import static robot.shooter.ShooterConstants.OPEN_SOLENOID_VALUE;
-import static robot.shooter.ShooterConstants.PISTON_CLOSE;
-import static robot.shooter.ShooterConstants.PISTON_OPEN;
 import static robot.shooter.ShooterConstants.ShooterComponentsA.MIDDLE_DISTANCE;
 import static robot.shooter.ShooterConstants.ShooterComponentsA.VELOCITY_PID_SLOT;
 import static robot.shooter.ShooterConstants.TOLERANCE;

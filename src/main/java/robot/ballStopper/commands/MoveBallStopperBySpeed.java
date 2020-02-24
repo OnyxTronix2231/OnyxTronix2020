dc995ce490ca329a21522b3b9e76c6edae93c571
yuval <yrashman@gmail.com>
2020-02-24 21:12:47 +0200

    add end in MoveBallStopper and MoveBallStopperDelay

--- src/main/java/robot/ballStopper/commands/MoveBallStopperBySpeed.java
+++ src/main/java/robot/ballStopper/commands/MoveBallStopperBySpeed.java
@@ -3,12 +3,13 @@ package robot.ballStopper.commands;
 import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import robot.ballStopper.BallStopper;
 
 import java.util.function.DoubleSupplier;
 
 public class MoveBallStopperBySpeed extends SequentialCommandGroup {
 
-  public MoveBallStopperBySpeed(robot.ballStopper.BallStopper ballStopper, DoubleSupplier ballStopperSpeed, double ballStopperDelay) {
+  public MoveBallStopperBySpeed(BallStopper ballStopper, DoubleSupplier ballStopperSpeed, double ballStopperDelay) {
     super(
         new MoveBallStopper(ballStopper, ballStopperSpeed).withTimeout(ballStopperDelay),
         parallel(

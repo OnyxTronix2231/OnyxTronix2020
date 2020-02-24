3bd3cbc544f81faf17b9a455f6cd6c62c82f735d
yuval <yrashman@gmail.com>
2020-02-24 18:54:53 +0200

    working

--- src/main/java/robot/ballStopper/BasicBallStopperComponentsA.java
+++ src/main/java/robot/ballStopper/BasicBallStopperComponentsA.java
@@ -3,20 +3,21 @@ package robot.ballStopper;
 import static robot.ballStopper.BallStopperConstants.BallStopperComponentsA.MASTER_MOTOR_PORT;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class BasicBallStopperComponentsA implements BallStopperComponents {
 
-  private final WPI_VictorSPX motor;
+  private final WPI_TalonSRX motor;
 
   public BasicBallStopperComponentsA() {
-    motor = new WPI_VictorSPX(MASTER_MOTOR_PORT);
+    motor = new WPI_TalonSRX(MASTER_MOTOR_PORT);
     motor.configFactoryDefault();
     motor.setNeutralMode(NeutralMode.Brake);
   }
 
   @Override
-  public WPI_VictorSPX getMotor() {
+  public WPI_TalonSRX getMotor() {
     return motor;
   }
 }

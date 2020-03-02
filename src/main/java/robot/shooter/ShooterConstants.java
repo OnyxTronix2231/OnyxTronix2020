c9ff28f01d6b10a002faa7193eaec578dc22a4bc
yuval <yrashman@gmail.com>
2020-03-02 21:56:01 +0200

    fix the button to shoot by velocity in TestingShooterOi

--- src/main/java/robot/shooter/ShooterConstants.java
+++ src/main/java/robot/shooter/ShooterConstants.java
@@ -23,6 +23,7 @@ public final class ShooterConstants {
     static final double CLOSE_LOOP_RAMP = 3;//TODO: check and change
   }
 
+  static final double TESTING_VELOCITY = 18000;
   static final boolean IS_PISTON_OPEN = false;
   static final int PERCENT_OUT_PUT = 1; //TODO check and change very important meters
   static final int TOLERANCE = 300;

59b741f58ace77e86f7dcb9017bb26b5b404a874
AE173 <s31079@tichon.shohamedu.org.il>
2020-03-07 17:55:17 +0200

    changed to whileActiveContiniuous

--- src/main/java/robot/leds/LedsOiBinder.java
+++ src/main/java/robot/leds/LedsOiBinder.java
@@ -13,14 +13,14 @@ public class LedsOiBinder {
   public LedsOiBinder(Shooter shooter, RGBLEDs rgbLeds, final JoystickAxis forwardAxis, final JoystickAxis rotateAxis,
                       final Trigger spinShooterAndAlignToTarget, final Trigger collectAndLoadBall,
                       final Trigger climb) {
-    forwardAxis.whenActive(new DriveColor(rgbLeds));
+    forwardAxis.whileActiveContinuous(new DriveColor(rgbLeds));
 
-    rotateAxis.whenActive(new DriveColor(rgbLeds));
+    rotateAxis.whileActiveContinuous(new DriveColor(rgbLeds));
 
-    spinShooterAndAlignToTarget.whenActive(new ShooterColor(shooter, rgbLeds));
+    spinShooterAndAlignToTarget.whileActiveContinuous(new ShooterColor(shooter, rgbLeds));
 
-    collectAndLoadBall.whenActive(new BallCollectorColor(rgbLeds));
+    collectAndLoadBall.whileActiveContinuous(new BallCollectorColor(rgbLeds));
 
-    climb.whenActive(new ClimberColor(rgbLeds));
+    climb.whileActiveContinuous(new ClimberColor(rgbLeds));
   }
 }

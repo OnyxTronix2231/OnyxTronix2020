82034a44e23905bb08ce02024796af46dfdd1ace
Shaked Cohen <27896110+shaked113021@users.noreply.github.com>
2020-03-09 19:05:31 +0200

    changed loading of balls

--- src/main/java/robot/crossSubsystem/ConveyorsOi.java
+++ src/main/java/robot/crossSubsystem/ConveyorsOi.java
@@ -2,11 +2,17 @@ package robot.crossSubsystem;
 
 import static robot.ballStopper.BallStopperConstants.DELAY;
 import static robot.ballStopper.BallStopperConstants.MOVE_STOPPER_BACK;
+import static robot.ballStopper.BallStopperConstants.PERCENTAGE_OUTPUT;
 import static robot.loaderConveyor.LoaderConveyorConstants.MOVE_LOADER_BACK;
+import static robot.loaderConveyor.LoaderConveyorConstants.PERCENTAGE_OUTPUT_MAX;
 import static robot.storageConveyor.StorageConveyorConstants.MOVE_STORAGE_BACK;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import onyxTronix.UniqueButtonCache;
 import robot.ballStopper.BallStopper;
@@ -14,6 +20,7 @@ import robot.ballStopper.commands.MoveBallStopperBySpeed;
 import robot.loaderConveyor.LoaderConveyor;
 import robot.loaderConveyor.commands.MoveLoaderConveyorBySpeed;
 import robot.storageConveyor.StorageConveyor;
+import robot.storageConveyor.StorageConveyorConstants;
 import robot.storageConveyor.commands.MoveStorageConveyorBySpeed;
 
 public class ConveyorsOi {
@@ -22,10 +29,26 @@ public class ConveyorsOi {
                      final StorageConveyor storageConveyor, final BallStopper ballStopper) {
     final Trigger moveConveyorsBack = driveJoystickButtonCache.createJoystickTrigger(
         XboxController.Button.kX.value);
-    moveConveyorsBack.whileActiveContinuous(new ParallelCommandGroup(
-        new MoveLoaderConveyorBySpeed(loaderConveyor, () -> MOVE_LOADER_BACK),
-        new MoveBallStopperBySpeed(ballStopper, () -> MOVE_STOPPER_BACK, DELAY),
-        new MoveStorageConveyorBySpeed(storageConveyor, () -> MOVE_STORAGE_BACK).withTimeout(0.2)
+    moveConveyorsBack.whileActiveContinuous(new SequentialCommandGroup(
+        new WaitCommand(0.2).raceWith(
+            new MoveLoaderConveyorBySpeed(loaderConveyor, () -> PERCENTAGE_OUTPUT_MAX)),
+        CommandGroupBase.race(new WaitCommand(0.3),
+            new MoveLoaderConveyorBySpeed(loaderConveyor, () -> PERCENTAGE_OUTPUT_MAX),
+            new MoveBallStopperBySpeed(ballStopper, () -> -PERCENTAGE_OUTPUT, DELAY),
+            new MoveStorageConveyorBySpeed(storageConveyor, () -> StorageConveyorConstants.PERCENTAGE_OUTPUT)
+        ),
+        CommandGroupBase.parallel(
+            CommandGroupBase.sequence(
+                CommandGroupBase.race(
+                    new WaitCommand(0.2),
+                    new MoveLoaderConveyorBySpeed(loaderConveyor, () -> PERCENTAGE_OUTPUT_MAX)
+                ),
+                new MoveLoaderConveyorBySpeed(loaderConveyor, () -> 0.6)
+            )
+            ,
+            new MoveBallStopperBySpeed(ballStopper, () -> PERCENTAGE_OUTPUT, DELAY),
+            new MoveStorageConveyorBySpeed(storageConveyor, () -> StorageConveyorConstants.PERCENTAGE_OUTPUT)
+        )
     ));
   }
 }

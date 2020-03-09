84e1338403b6d175397fa1049446a717c239a959
AE173 <s31079@tichon.shohamedu.org.il>
2020-03-09 15:25:06 +0200

    Moved constant Paths to AutonomousConstants, added and deleted some constants and deleted TempOi as it did nothing at all

--- src/main/java/robot/autonomous/AutonomousConstants.java
+++ src/main/java/robot/autonomous/AutonomousConstants.java
@@ -1,11 +1,85 @@
 package robot.autonomous;
 
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import robot.drivetrain.Path;
+
+import java.util.List;
+
 public final class AutonomousConstants {
 
-  public static final double LOADER_VELOCITY = 0.8;
-  public static final double BALL_STOPPER_VELOCITY = 0.8;
   public static final double AUTONOMOUS_DISTANCE = 25;
   public static final double DRIVE_AUTONOMOUS_TIMEOUT = 5;
-  public static final double STORAGE_VELOCITY = 0.8;
-  public static final double TIMER = 7;
+  public static final double SHOOTER_TIMER = 3;
+
+  public static final class Paths {
+
+    public static final Path[][] PATHS = {START_INFRONT_OF_TARGET_COLLECT_THREE_BALLS_FROM_SHIELD_GENERATOR(),
+        START_INFRONT_OF_TRENCH_COLLECT_TWO_BALLS_SHOOT_COLLECT_THREE_MORE()};
+
+    public static Path[] START_INFRONT_OF_TARGET_COLLECT_THREE_BALLS_FROM_SHIELD_GENERATOR() {
+      final List<Pose2d> subPathOnePoints = List.of(
+          new Pose2d(3.2, 2.4, Rotation2d.fromDegrees(90)),
+          new Pose2d(5.8, 4.4, Rotation2d.fromDegrees(-20))
+      );
+      final Path subPathOne = new Path(true, subPathOnePoints);
+
+      final List<Pose2d> subPathTwoPoints = List.of(
+          subPathOne.getEndingPose(),
+          new Pose2d(5, 4.3, Rotation2d.fromDegrees(20))
+      );
+      final Path subPathTwo = new Path(false, subPathTwoPoints);
+
+      final List<Pose2d> subPathThreePoints = List.of(
+          subPathTwo.getEndingPose(),
+          new Pose2d(5.6, 3.9, Rotation2d.fromDegrees(-20))
+      );
+      final Path subPathThree = new Path(true, subPathThreePoints);
+
+      final List<Pose2d> subPathFourPoints = List.of(
+          subPathThree.getEndingPose(),
+          new Pose2d(3.65, 3, Rotation2d.fromDegrees(120))
+      );
+      final Path subPathFour = new Path(false, subPathFourPoints);
+
+      return new Path[]{subPathOne, subPathTwo, subPathThree, subPathFour};
+    }
+
+    static Path[] START_INFRONT_OF_TRENCH_COLLECT_TWO_BALLS_SHOOT_COLLECT_THREE_MORE() {
+      final List<Pose2d> subPathOnePoints = List.of(
+          new Pose2d(3, 0.7, Rotation2d.fromDegrees(0)),
+          new Pose2d(7, 0.7, Rotation2d.fromDegrees(0))
+      );
+      final Path subPathOne = new Path(true, subPathOnePoints);
+
+      final List<Pose2d> subPathTwoPoints = List.of(
+          subPathOne.getEndingPose(),
+          new Pose2d(8, 0.7, Rotation2d.fromDegrees(0))
+      );
+      final Path subPathTwo = new Path(true, subPathTwoPoints);
+
+      final List<Pose2d> subPathThreePoints = List.of(
+          subPathTwo.getEndingPose(),
+          new Pose2d(3, 2.4, Rotation2d.fromDegrees(0))
+      );
+      final Path subPathThree = new Path(false, subPathThreePoints);
+      return new Path[]{subPathOne, subPathThree};
+    }
+
+    public static Path[] TEST_PATH() {
+      final List<Pose2d> test = List.of(
+          new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+          new Pose2d(1, 0, Rotation2d.fromDegrees(0))
+      );
+      final Path subPathOne = new Path(true, test);
+
+      final List<Pose2d> subPathTwoPoints = List.of(
+          new Pose2d(1, 0, Rotation2d.fromDegrees(0)),
+          new Pose2d(0, 0, Rotation2d.fromDegrees(0))
+      );
+      final Path subPathTwo = new Path(false, subPathTwoPoints);
+
+      return new Path[]{subPathOne, subPathTwo};
+    }
+  }
 }
--- src/main/java/robot/drivetrain/commands/FirstPath.java
+++ src/main/java/robot/drivetrain/commands/FirstPath.java
@@ -1,9 +1,9 @@
 package robot.drivetrain.commands;
 
+import static robot.autonomous.AutonomousConstants.SHOOTER_TIMER;
 import static robot.ballStopper.BallStopperConstants.PERCENTAGE_OUTPUT;
-import static robot.drivetrain.DriveTrainConstants.Paths;
-
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import robot.autonomous.AutonomousConstants.Paths;
 import robot.ballCollector.BallCollector;
 import robot.ballCollector.commands.CloseBallCollectorPistons;
 import robot.ballCollector.commands.CollectBallBySpeed;
@@ -24,17 +24,17 @@ public class FirstPath extends SequentialCommandGroup {
                    final BallCollector ballCollector, final LoaderConveyor loaderConveyor,
                    final StorageConveyor storageConveyor, final BallStopper ballStopper) {
     super(
-        new ShootByDistance(shooter, () -> vision.getOuterTarget().getDistance()).withTimeout(3),
+        new ShootByDistance(shooter, () -> vision.getOuterTarget().getDistance()).withTimeout(SHOOTER_TIMER),
         sequence(
-            new MoveToPose(driveTrain, Paths.FIRST_PATH()[0].getEndingPose()),
-            new MoveToPose(driveTrain, Paths.FIRST_PATH()[1].getEndingPose()),
-            new MoveToPose(driveTrain, Paths.FIRST_PATH()[2].getEndingPose()))
+            new MoveToPose(driveTrain, Paths.START_INFRONT_OF_TARGET_COLLECT_THREE_BALLS_FROM_SHIELD_GENERATOR()[0].getEndingPose()),
+            new MoveToPose(driveTrain, Paths.START_INFRONT_OF_TARGET_COLLECT_THREE_BALLS_FROM_SHIELD_GENERATOR()[1].getEndingPose()),
+            new MoveToPose(driveTrain, Paths.START_INFRONT_OF_TARGET_COLLECT_THREE_BALLS_FROM_SHIELD_GENERATOR()[2].getEndingPose()))
             .deadlineWith(
             new OpenAndCollect(new OpenBallCollectorPistons(ballCollector),
                 new CollectBallBySpeed(ballCollector, () -> PERCENTAGE_OUTPUT)),
             new LoadBall(loaderConveyor, storageConveyor, ballStopper)),
         new CloseBallCollectorPistons(ballCollector),
-        new MoveToPose(driveTrain, Paths.FIRST_PATH()[3].getEndingPose()),
-        new ShootByDistance(shooter, () -> vision.getOuterTarget().getDistance()).withTimeout(3));
+        new MoveToPose(driveTrain, Paths.START_INFRONT_OF_TARGET_COLLECT_THREE_BALLS_FROM_SHIELD_GENERATOR()[3].getEndingPose()),
+        new ShootByDistance(shooter, () -> vision.getOuterTarget().getDistance()).withTimeout(SHOOTER_TIMER));
   }
 }

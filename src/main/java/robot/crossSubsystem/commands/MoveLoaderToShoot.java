3118d9f743c98bab00e599bee8d5add95581f9d9
Shaked Cohen <27896110+shaked113021@users.noreply.github.com>
2020-03-09 19:12:38 +0200

    move command to ideal place

---
+++ src/main/java/robot/crossSubsystem/commands/MoveLoaderToShoot.java
@@ -0,0 +1,42 @@
+package robot.crossSubsystem.commands;
+
+import static robot.ballStopper.BallStopperConstants.DELAY;
+import static robot.ballStopper.BallStopperConstants.PERCENTAGE_OUTPUT;
+import static robot.loaderConveyor.LoaderConveyorConstants.PERCENTAGE_OUTPUT_MAX;
+
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import robot.ballStopper.BallStopper;
+import robot.ballStopper.commands.MoveBallStopperBySpeed;
+import robot.loaderConveyor.LoaderConveyor;
+import robot.loaderConveyor.commands.MoveLoaderConveyorBySpeed;
+import robot.storageConveyor.StorageConveyor;
+import robot.storageConveyor.StorageConveyorConstants;
+import robot.storageConveyor.commands.MoveStorageConveyorBySpeed;
+
+public class MoveLoaderToShoot extends SequentialCommandGroup {
+
+  public MoveLoaderToShoot(final LoaderConveyor loaderConveyor, final BallStopper ballStopper,
+                           final StorageConveyor storageConveyor) {
+    super(
+        new WaitCommand(0.2).raceWith(
+            new MoveLoaderConveyorBySpeed(loaderConveyor, () -> PERCENTAGE_OUTPUT_MAX)),
+        CommandGroupBase.race(new WaitCommand(0.3),
+            new MoveLoaderConveyorBySpeed(loaderConveyor, () -> PERCENTAGE_OUTPUT_MAX),
+            new MoveBallStopperBySpeed(ballStopper, () -> -PERCENTAGE_OUTPUT, DELAY),
+            new MoveStorageConveyorBySpeed(storageConveyor, () -> StorageConveyorConstants.PERCENTAGE_OUTPUT)
+        ),
+        CommandGroupBase.parallel(
+            CommandGroupBase.sequence(
+                CommandGroupBase.race(
+                    new WaitCommand(0.2),
+                    new MoveLoaderConveyorBySpeed(loaderConveyor, () -> PERCENTAGE_OUTPUT_MAX)
+                ),
+                new MoveLoaderConveyorBySpeed(loaderConveyor, () -> 0.6)
+            )
+            ,
+            new MoveBallStopperBySpeed(ballStopper, () -> PERCENTAGE_OUTPUT, DELAY),
+            new MoveStorageConveyorBySpeed(storageConveyor, () -> StorageConveyorConstants.PERCENTAGE_OUTPUT)));
+  }
+}

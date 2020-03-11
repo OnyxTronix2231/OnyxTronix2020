962a98527eeddcc7d83b89aee1ec903ba32c8693
Shaked Cohen <27896110+shaked113021@users.noreply.github.com>
2020-03-11 18:58:04 +0200

    changed vals to correct vals

--- src/main/java/robot/vision/VisionConstants.java
+++ src/main/java/robot/vision/VisionConstants.java
@@ -14,8 +14,8 @@ public final class VisionConstants {
   public static final double MAX_INNER_ORIENTATION = 18;
 
   public static final class RobotAConstants {
-    public static final double CAMERA_VERTICAL_OFFSET_ANGLE = 34.08; // TODO: Check and change
-    public static final double CAMERA_HEIGHT_CM = 55; //TODO: Check and change
+    public static final double CAMERA_VERTICAL_OFFSET_ANGLE = 32; // TODO: Check and change
+    public static final double CAMERA_HEIGHT_CM = 54.7; //TODO: Check and change
     public static final double LIMELIGHT_TURRET_CENTER_CM = 22.5;
     public static final Vector2dEx VECTOR_LIMELIGHT_TURRET_CENTER = new Vector2dEx(LIMELIGHT_TURRET_CENTER_CM, 0);
     public static final double ROBOT_CENTER_TURRET_DISTANCE = 18; // TODO: Check and change

39915665d0e8c6318673657da6c0afc6b64af59d
NadaVibe <nadav8775@gmail.com>
2020-08-22 02:13:00 +0300

    Changed the open loop and closed loop ramp of the shooter so it won't destroy the shooter

--- src/main/java/robot/shooter/ShooterConstants.java
+++ src/main/java/robot/shooter/ShooterConstants.java
@@ -20,8 +20,8 @@ public final class ShooterConstants {
     static final double VELOCITY_I = 0;
     static final double VELOCITY_D = 50;
     static final double VELOCITY_F = MAX_CLOSED_LOOP_OUTPUT / MAX_VELOCITY;
-    static final double OPEN_LOOP_RAMP = 0;//TODO: check and change
-    static final double CLOSE_LOOP_RAMP = 0;//3;//TODO: check and change
+    static final double OPEN_LOOP_RAMP = 5;//TODO: check and change
+    static final double CLOSE_LOOP_RAMP = 5;//3;//TODO: check and change
     static final double MIN_VELOCITY_ERROR = 1000;
     static final double AT_SHOOTING_VELOCITY = 400;
   }
